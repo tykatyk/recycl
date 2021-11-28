7de996f3b675ea23071997b88ba1175c62c2a638
tykatyk <tykatyk@gmail.com>
2021-11-28 12:39:13 +0200

    Refactor: avoid multiple instances of Appolo Server

--- pages/api/g.js
+++ pages/api/g.js
@@ -3,8 +3,9 @@ import dbConnect from '../../lib/db/connection'
 import schema from '../../lib/graphql/schema'
 import resolvers from '../../lib/graphql/resolvers'
 
-// const apolloServer = new ApolloServer({ typeDefs, resolvers })
-// const startServer = apolloServer.start()
+const apolloServer = new ApolloServer({ typeDefs: schema, resolvers })
+const startServer = apolloServer.start()
+
 async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Credentials', 'true')
   res.setHeader('Access-Control-Allow-Origin', `${process.env.NEXT_PUBLIC_URL}`)
@@ -16,14 +17,13 @@ async function handler(req, res) {
     res.end()
     return false
   }
-  const apolloServer = new ApolloServer({ typeDefs: schema, resolvers })
-  const startServer = apolloServer.start()
 
   await startServer
   await apolloServer.createHandler({
     path: '/api/g',
   })(req, res)
 }
+
 export default dbConnect(handler)
 export const config = {
   api: {
