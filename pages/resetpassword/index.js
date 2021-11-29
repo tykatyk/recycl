3872ecfb1d234272495c8033b2bf44c9cc8d130d
tykatyk <tykatyk@gmail.com>
2021-11-29 09:55:03 +0200

    Refactor route paths for reset password page

--- pages/resetpassword/index.js
+++ pages/resetpassword/index.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import ResetPasswordPage from '../client/ResetPasswordPage.jsx'
+import ResetPasswordPage from '../../client/ResetPasswordPage.jsx'
 
 export default function ResetPassword() {
   return <ResetPasswordPage />
