792c579868fe39af3632f0c375764909e0f206ad
tykatyk <tykatyk@gmail.com>
2021-11-29 12:08:36 +0200

    Refactor register page to use auth layout

--- client/RegisterPage.jsx
+++ client/RegisterPage.jsx
@@ -2,9 +2,6 @@ import React, { useState } from 'react'
 import {
   Avatar,
   Button,
-  TextField,
-  FormControlLabel,
-  Checkbox,
   Grid,
   Box,
   Typography,
@@ -19,7 +16,6 @@ import PageLoadingCircle from './uiParts/PageLoadingCircle.jsx'
 import ButtonSubmittingCircle from './uiParts/ButtonSubmittingCircle.jsx'
 import Snackbar from './uiParts/Snackbars.jsx'
 import Link from './uiParts/Link.jsx'
-import Head from './uiParts/Head.jsx'
 import Copyright from './uiParts/Copyright.jsx'
 import * as yup from 'yup'
 import { signIn } from 'next-auth/react'
@@ -27,6 +23,7 @@ import { GET_ROLE_ID } from '../lib/graphql/queries/userRole'
 import { useQuery } from '@apollo/client'
 import { useRouter } from 'next/router'
 import { registerSchema } from '../lib/validation/'
+import AuthLayout from './layouts/AuthLayout.jsx'
 
 const useStyles = makeStyles((theme) => ({
   paper: {
@@ -65,140 +62,141 @@ export default function SignUp() {
 
   return (
     <>
-      <Head title="Recycl | Register" />
-      <Container component="main" maxWidth="xs">
-        <div className={classes.paper}>
-          <Avatar className={classes.avatar}>
-            <LockOutlinedIcon />
-          </Avatar>
-          <Typography component="h1" variant="h5">
-            Регистрация
-          </Typography>
-          <Formik
-            initialValues={{
-              role: data.getRoleId,
-              username: '',
-              email: '',
-              password: '',
-              confirmPassword: '',
-            }}
-            validationSchema={registerSchema}
-            onSubmit={(values, { setSubmitting, setErrors }) => {
-              setSubmitting(true)
+      <AuthLayout title="Recycl | Register">
+        <Container component="main" maxWidth="xs">
+          <div className={classes.paper}>
+            <Avatar className={classes.avatar}>
+              <LockOutlinedIcon />
+            </Avatar>
+            <Typography component="h1" variant="h5">
+              Регистрация
+            </Typography>
+            <Formik
+              initialValues={{
+                role: data.getRoleId,
+                username: '',
+                email: '',
+                password: '',
+                confirmPassword: '',
+              }}
+              validationSchema={registerSchema}
+              onSubmit={(values, { setSubmitting, setErrors }) => {
+                setSubmitting(true)
 
-              fetch('/api/auth/signup/', {
-                method: 'POST',
-                body: JSON.stringify(values),
-                headers: {
-                  'Content-Type': 'application/json',
-                },
-              })
-                .then((response) => {
-                  return response.json()
+                fetch('/api/auth/signup/', {
+                  method: 'POST',
+                  body: JSON.stringify(values),
+                  headers: {
+                    'Content-Type': 'application/json',
+                  },
                 })
-                .then((data) => {
-                  if (data.error) {
-                    if (data.error.type === 'perField') {
-                      setErrors(data.error.message)
+                  .then((response) => {
+                    return response.json()
+                  })
+                  .then((data) => {
+                    if (data.error) {
+                      if (data.error.type === 'perField') {
+                        setErrors(data.error.message)
+                        return
+                      }
+                      if (data.error.type === 'perForm') {
+                        setBackendError(data.error.message)
+                        return
+                      }
+
+                      setBackendError(
+                        'Неизвестная ошибка при обработке ответа сервера'
+                      )
                       return
                     }
-                    if (data.error.type === 'perForm') {
-                      setBackendError(data.error.message)
-                      return
-                    }
-
-                    setBackendError(
-                      'Неизвестная ошибка при обработке ответа сервера'
-                    )
-                    return
-                  }
-                  router.push('/')
-                })
-                .catch((error) => {
-                  setBackendError('Ошибка при отпавке запроса на сервер')
-                })
-                .finally(() => {
-                  setSubmitting(false)
-                })
-            }}
-          >
-            {({ isSubmitting, errors }) => {
-              return (
-                <Form className={classes.form} noValidate autoComplete="off">
-                  <Field type="hidden" name="role" />
-                  <Field
-                    variant="outlined"
-                    margin="normal"
-                    required
-                    fullWidth
-                    id="username"
-                    label="Имя или название организации"
-                    name="username"
-                    component={TextFieldFormik}
-                  />
-                  <Field
-                    variant="outlined"
-                    margin="normal"
-                    required
-                    fullWidth
-                    id="email"
-                    label="Электронная почта"
-                    name="email"
-                    component={TextFieldFormik}
-                  />
-                  <Field
-                    variant="outlined"
-                    margin="normal"
-                    required
-                    fullWidth
-                    name="password"
-                    label="Пароль"
-                    type="password"
-                    id="password"
-                    component={TextFieldFormik}
-                  />
-                  <Field
-                    variant="outlined"
-                    margin="normal"
-                    required
-                    fullWidth
-                    name="confirmPassword"
-                    label="Пароль повторно"
-                    type="password"
-                    id="confirmPassword"
-                    component={TextFieldFormik}
-                  />
-                  <Button
-                    type="submit"
-                    fullWidth
-                    variant="contained"
-                    color="secondary"
-                    className={classes.submit}
-                    disabled={isSubmitting}
-                  >
-                    Зарегистрироваться
-                    {isSubmitting && <ButtonSubmittingCircle />}
-                  </Button>
-                  <Grid container style={{ justifyContent: 'flex-end' }}>
-                    <Grid item>
-                      <Link
-                        href="/login"
-                        variant="body2"
-                        style={{ color: `${theme.palette.text.secondary}` }}
-                      >
-                        {'Уже есть аккаунт?'}
-                      </Link>
+                    router.push('/')
+                  })
+                  .catch((error) => {
+                    setBackendError('Ошибка при отпавке запроса на сервер')
+                  })
+                  .finally(() => {
+                    setSubmitting(false)
+                  })
+              }}
+            >
+              {({ isSubmitting }) => {
+                return (
+                  <Form className={classes.form} noValidate autoComplete="off">
+                    <Field type="hidden" name="role" />
+                    <Field
+                      variant="outlined"
+                      margin="normal"
+                      required
+                      fullWidth
+                      id="username"
+                      label="Имя или название организации"
+                      name="username"
+                      component={TextFieldFormik}
+                    />
+                    <Field
+                      variant="outlined"
+                      margin="normal"
+                      required
+                      fullWidth
+                      id="email"
+                      label="Электронная почта"
+                      name="email"
+                      component={TextFieldFormik}
+                    />
+                    <Field
+                      variant="outlined"
+                      margin="normal"
+                      required
+                      fullWidth
+                      name="password"
+                      label="Пароль"
+                      type="password"
+                      id="password"
+                      component={TextFieldFormik}
+                    />
+                    <Field
+                      variant="outlined"
+                      margin="normal"
+                      required
+                      fullWidth
+                      name="confirmPassword"
+                      label="Пароль повторно"
+                      type="password"
+                      id="confirmPassword"
+                      component={TextFieldFormik}
+                    />
+                    <Button
+                      type="submit"
+                      fullWidth
+                      variant="contained"
+                      color="secondary"
+                      className={classes.submit}
+                      disabled={isSubmitting}
+                    >
+                      Зарегистрироваться
+                      {isSubmitting && <ButtonSubmittingCircle />}
+                    </Button>
+                    <Grid container style={{ justifyContent: 'flex-end' }}>
+                      <Grid item>
+                        <Link
+                          href="/login"
+                          variant="body2"
+                          style={{ color: `${theme.palette.text.secondary}` }}
+                        >
+                          {'Уже есть аккаунт?'}
+                        </Link>
+                      </Grid>
                     </Grid>
-                  </Grid>
-                </Form>
-              )
-            }}
-          </Formik>
-        </div>
-        <Box mt={8}>
-          <Copyright />
-        </Box>
-      </Container>
+                  </Form>
+                )
+              }}
+            </Formik>
+          </div>
+          <Box mt={8}>
+            <Copyright />
+          </Box>
+        </Container>
+      </AuthLayout>
       <Snackbar
         severity="error"
         open={!!backendError}
