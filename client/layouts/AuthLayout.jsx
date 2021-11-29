2b36bf49db335ccf96e4fb66eb9ff49f71ead85c
tykatyk <tykatyk@gmail.com>
2021-11-29 09:48:56 +0200

    Add new layout for auth pages

---
+++ client/layouts/AuthLayout.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Wrapper from './Wrapper.jsx'
+import Footer from '../uiParts/Footer.jsx'
+import Head from '../uiParts/Head.jsx'
+import BackButton from '../uiParts/BackButton.jsx'
+
+export default function AuthLayout({ children, title }) {
+  return (
+    <>
+      <Head title={title} />
+      <Wrapper>
+        <BackButton />
+        <main>{children}</main>
+        <Footer />
+      </Wrapper>
+    </>
+  )
+}
