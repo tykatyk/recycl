0309cc679a5e4d3fcfffaf248f61aaf3d64292ea
tykatyk <tykatyk@gmail.com>
2021-11-29 10:14:38 +0200

    Drop route for reset password error
    This logic is now in ResetPasswordTokenPage

--- pages/resetpassworderror.js
+++
@@ -1,6 +0,0 @@
-import React from 'react'
-import ResetPasswordErrorPage from '../client/ResetPasswordErrorPage.jsx'
-
-export default function ResetPasswordError() {
-  return <ResetPasswordErrorPage />
-}
