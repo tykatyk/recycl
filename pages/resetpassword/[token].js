32508e99bed9e5c44c821d526c0e6bddb2f2c642
tykatyk <tykatyk@gmail.com>
2021-11-29 10:11:27 +0200

    Start develop UI route to check reset pswd token

---
+++ pages/resetpassword/[token].js
@@ -0,0 +1,6 @@
+import React from 'react'
+import ResetPasswordTokenPage from '../../client/ResetPasswordTokenPage.jsx'
+
+export default function ResetPasswordToken() {
+  return <ResetPasswordTokenPage />
+}
