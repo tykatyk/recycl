f3a023893d6ca745678629ca3a6877611b31d238
tykatyk <tykatyk@gmail.com>
2021-12-02 22:19:12 +0200

    Add UI for user's settings page

---
+++ pages/myaccount/settings.js
@@ -0,0 +1,77 @@
+import React from 'react'
+import {
+  Accordion,
+  AccordionSummary,
+  AccordionDetails,
+  Container,
+  Typography,
+  makeStyles,
+} from '@material-ui/core'
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore'
+import Layout from '../../client/layouts/Layout.jsx'
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    width: '100%',
+  },
+  heading: {
+    fontSize: theme.typography.pxToRem(15),
+    fontWeight: theme.typography.fontWeightRegular,
+  },
+}))
+
+export default function SimpleAccordion() {
+  const classes = useStyles()
+
+  return (
+    <Layout title="Recycl | Мои настройки">
+      <Container component="main" maxWidth="md">
+        <div className={classes.root}>
+          <Accordion>
+            <AccordionSummary
+              expandIcon={<ExpandMoreIcon />}
+              aria-controls="panel1a-content"
+              id="panel1a-header"
+            >
+              <Typography className={classes.heading}>Accordion 1</Typography>
+            </AccordionSummary>
+            <AccordionDetails>
+              <Typography>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                Suspendisse malesuada lacus ex, sit amet blandit leo lobortis
+                eget.
+              </Typography>
+            </AccordionDetails>
+          </Accordion>
+          <Accordion>
+            <AccordionSummary
+              expandIcon={<ExpandMoreIcon />}
+              aria-controls="panel2a-content"
+              id="panel2a-header"
+            >
+              <Typography className={classes.heading}>Accordion 2</Typography>
+            </AccordionSummary>
+            <AccordionDetails>
+              <Typography>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                Suspendisse malesuada lacus ex, sit amet blandit leo lobortis
+                eget.
+              </Typography>
+            </AccordionDetails>
+          </Accordion>
+          <Accordion disabled>
+            <AccordionSummary
+              expandIcon={<ExpandMoreIcon />}
+              aria-controls="panel3a-content"
+              id="panel3a-header"
+            >
+              <Typography className={classes.heading}>
+                Disabled Accordion
+              </Typography>
+            </AccordionSummary>
+          </Accordion>
+        </div>
+      </Container>
+    </Layout>
+  )
+}
