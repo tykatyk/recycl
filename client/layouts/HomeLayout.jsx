91f817c933df755f30d581b5713a26ee65cb2578
tykatyk <tykatyk@gmail.com>
2021-12-05 21:05:16 +0200

    Add separate layout for home page

---
+++ client/layouts/HomeLayout.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Wrapper from '../uiParts/Wrapper.jsx'
+import Header from '../uiParts/header/Header.jsx'
+import Footer from '../uiParts/Footer.jsx'
+import Head from '../uiParts/Head.jsx'
+
+export default function Layout({ children, title }) {
+  return (
+    <>
+      <Head title={title} />
+      <Wrapper>
+        <Header />
+        <main>{children}</main>
+        <Footer />
+      </Wrapper>
+    </>
+  )
+}
