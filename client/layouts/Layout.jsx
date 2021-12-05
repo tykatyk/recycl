ab76c6c78dd954e784df8275006e962b9fb52253
tykatyk <tykatyk@gmail.com>
2021-12-05 21:04:49 +0200

    Add container component to main layout

--- client/layouts/Layout.jsx
+++ client/layouts/Layout.jsx
@@ -1,16 +1,27 @@
 import React from 'react'
+import { Container, makeStyles } from '@material-ui/core'
 import Wrapper from '../uiParts/Wrapper.jsx'
 import Header from '../uiParts/header/Header.jsx'
 import Footer from '../uiParts/Footer.jsx'
 import Head from '../uiParts/Head.jsx'
 
+const useStyles = makeStyles((theme) => ({
+  root: {
+    padding: theme.spacing(6),
+  },
+}))
+
 export default function Layout({ children, title }) {
+  const classes = useStyles()
+
   return (
     <>
       <Head title={title} />
       <Wrapper>
         <Header />
-        <main>{children}</main>
+        <Container className={classes.root} component="div" maxWidth="md">
+          <main>{children}</main>
+        </Container>
         <Footer />
       </Wrapper>
     </>
