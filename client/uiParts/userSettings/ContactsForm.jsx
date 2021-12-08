c56f69431ff11719c3b6d9b71fd49d9b34d29b65
tykatyk <tykatyk@gmail.com>
2021-12-08 12:06:24 +0200

    Refactor showing errors logic

--- client/uiParts/userSettings/ContactsForm.jsx
+++ client/uiParts/userSettings/ContactsForm.jsx
@@ -33,92 +33,85 @@ export default function ContactsForm() {
   const classes = useStyles()
   const theme = useTheme()
   const [backendError, setBackendError] = useState(null)
-  const [successMessage, setSuccessMessage] = useState(null)
-  const [severity, setSeverity] = useState('error')
 
   const [updateContacts, { data, loading, error }] =
     useMutation(UPDATE_USER_CONTACTS)
 
   useEffect(() => {
-    if (backendError) {
-      setSeverity('error')
-      setBackendError(backendError)
-    } else if (successMessage) {
-      setSeverity('success')
-      setSuccessMessage(successMessage)
+    if (error) {
+      setBackendError(error)
     }
-  }, [backendError, successMessage])
+  }, [error])
 
   return (
     <Box className={classes.box}>
-        <Formik
-          initialValues={{
-            username: '',
-            location: '',
-          }}
-          validationSchema={contactsSchema}
+      <Formik
+        initialValues={{
+          username: '',
+          location: '',
+        }}
+        validationSchema={contactsSchema}
         onSubmit={async (values, { setSubmitting, setErrors }) => {
-            setSubmitting(true)
+          setSubmitting(true)
           const location = {
             description: values.location.description,
             place_id: values.location.place_id,
-                }
+          }
           values.location = location
 
           await updateContacts({ variables: { contacts: values } })
-                setSubmitting(false)
-          }}
-        >
-          {({ isSubmitting }) => {
-            return (
-              <Form className={classes.form} noValidate autoComplete="off">
-                <Field
-                  variant="outlined"
-                  margin="normal"
-                  required
-                  fullWidth
-                  id="username"
-                  label="Имя или название организации"
-                  name="username"
-                  component={TextFieldFormik}
-                  className={classes.field}
-                />
-                <Field
-                  variant="outlined"
-                  margin="normal"
-                  required
-                  fullWidth
-                  id="location"
-                  label="Город"
-                  name="location"
-                  component={PlacesAutocomplete}
-                  className={classes.field}
-                  backgroundColor={theme.palette.grey['800']}
-                />
-                <Button
-                  type="submit"
-                  fullWidth
-                  variant="contained"
-                  color="secondary"
-                  className={classes.submit}
-                  disabled={isSubmitting}
-                  style={{ width: 'auto' }}
-                >
-                  Сохранить
-                  {isSubmitting && <ButtonSubmittingCircle />}
-                </Button>
-              </Form>
-            )
-          }}
-        </Formik>
+          setSubmitting(false)
+        }}
+      >
+        {({ isSubmitting }) => {
+          return (
+            <Form className={classes.form} noValidate autoComplete="off">
+              <Field
+                variant="outlined"
+                margin="normal"
+                required
+                fullWidth
+                id="username"
+                label="Имя или название организации"
+                name="username"
+                component={TextFieldFormik}
+                className={classes.field}
+              />
+              <Field
+                variant="outlined"
+                margin="normal"
+                required
+                fullWidth
+                id="location"
+                label="Город"
+                name="location"
+                component={PlacesAutocomplete}
+                className={classes.field}
+                backgroundColor={theme.palette.grey['800']}
+              />
+              <Button
+                type="submit"
+                fullWidth
+                variant="contained"
+                color="secondary"
+                className={classes.submit}
+                disabled={isSubmitting}
+                style={{ width: 'auto' }}
+              >
+                Сохранить
+                {isSubmitting && <ButtonSubmittingCircle />}
+              </Button>
+            </Form>
+          )
+        }}
+      </Formik>
 
       <Snackbar
-        severity={severity}
-        open={!!backendError || !!successMessage}
-        message={backendError || successMessage}
+        severity="error"
+        open={backendError}
+        message="Возникла ошибка при сохранении данных"
         handleClose={() => {
           setBackendError(null)
-          setSuccessMessage(null)
         }}
       />
     </Box>
