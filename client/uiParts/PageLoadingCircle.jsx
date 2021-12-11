8a70ab0907ff1d797c446bcc3a73c022b1702f96
tykatyk <tykatyk@gmail.com>
2021-12-11 20:32:29 +0200

    Add possibility to accespt style prop
    By page loading component

--- client/uiParts/PageLoadingCircle.jsx
+++ client/uiParts/PageLoadingCircle.jsx
@@ -1,7 +1,15 @@
 import React from 'react'
 import { CircularProgress } from '@material-ui/core'
 
-export default function PageLoadingCircle() {
+export default function PageLoadingCircle({ style }) {
+  if (style) {
+    return (
+      <div style={style}>
+        <CircularProgress color="secondary" />
+      </div>
+    )
+  }
+
   return (
     <div
       style={{
