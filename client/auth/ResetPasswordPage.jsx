e6f299b2d9bc64dc35aa2eba464b28b77c6d63fe
tykatyk <tykatyk@gmail.com>
2021-12-14 19:53:54 +0200

    Rearrange layout

--- client/auth/ResetPasswordPage.jsx
+++ client/auth/ResetPasswordPage.jsx
@@ -5,7 +5,7 @@ import { GET_USER_BY_TOKEN } from '../../lib/graphql/queries/user'
 import { useQuery } from '@apollo/client'
 import PageLoadingCircle from '../uiParts/PageLoadingCircle.jsx'
 import Head from '../uiParts/Head.jsx'
-import AuthLayout from '../layouts/AuthLayout.jsx'
+import ResetLayout from '../layouts/ResetLayout.jsx'
 import ResetPasswordForm from './ResetPasswordForm.jsx'
 
 export default function ResetPasswordPage({ token }) {
@@ -21,35 +21,11 @@ export default function ResetPasswordPage({ token }) {
       </>
     )
 
-  if (error) return <Content message="Возникла ошибка при проверке токена" />
+  if (error)
+    return <ResetLayout message="Возникла ошибка при проверке токена" />
 
-  if (!data.getByToken) return <Content message="Срок действия ссылки истек" />
+  if (!data.getByToken)
+    return <ResetLayout message="Срок действия ссылки истек" />
 
   return <ResetPasswordForm token={token} />
-}
-
-const Content = (props) => {
-  return (
-    <AuthLayout title="Recycl | Error">
-      <Grid
-        container
-        alignItems="center"
-        direction="column"
-        style={{ flex: '1 0 auto' }}
-      >
-        <Grid item>
-          <Grid
-            container
-            direction="column"
-            alignItems="center"
-            justify="center"
-          >
-            <Alert variant="filled" severity="error">
-              {props.message}
-            </Alert>
-          </Grid>
-        </Grid>
-      </Grid>
-    </AuthLayout>
-  )
 }
