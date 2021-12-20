d32e1ab38bee23b79de29dad96c8eaa987d07fe7
tykatyk <tykatyk@gmail.com>
2021-12-20 23:19:09 +0200

    Add title

--- client/uiParts/userSettings/ChangeEmailPage.jsx
+++ client/uiParts/userSettings/ChangeEmailPage.jsx
@@ -2,10 +2,13 @@ import React from 'react'
 import ResetLayout from '../../layouts/ResetLayout.jsx'
 
 export default function ChangeEmailPage({ urlIsValid }) {
-  if (!urlIsValid) return <ResetLayout message="Срок действия ссылки истек" />
+  const title = 'Смена email'
+  if (!urlIsValid)
+    return <ResetLayout title={title} message="Срок действия ссылки истек" />
 
   return (
     <ResetLayout
+      title={title}
       message="Адрес электронной почты успешно изменен"
       severity="success"
     />
