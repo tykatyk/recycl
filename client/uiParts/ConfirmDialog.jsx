745960c993de93b454481fd7109aa18fb4df0a7e
tykatyk <tykatyk@gmail.com>
2021-12-21 20:48:37 +0200

    Add UI to confirm an action

---
+++ client/uiParts/ConfirmDialog.jsx
@@ -0,0 +1,43 @@
+import React, { useEffect, useState } from 'react'
+import {
+  Button,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+  DialogTitle,
+} from '@material-ui/core'
+
+export default function ConfirmDialog(props) {
+  const {
+    title = '',
+    message = '',
+    open = false,
+    handleConfirm = null,
+    handleReject = null,
+  } = props
+  return (
+    <div>
+      <Dialog
+        open={open}
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
+      >
+        <DialogTitle id="alert-dialog-title">{title}</DialogTitle>
+        <DialogContent>
+          <DialogContentText id="alert-dialog-description">
+            {message}
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button color="secondary" onClick={handleReject}>
+            Нет
+          </Button>
+          <Button color="secondary" onClick={handleConfirm}>
+            Да
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </div>
+  )
+}
