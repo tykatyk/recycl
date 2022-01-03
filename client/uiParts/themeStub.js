924f2f8f23b6ca00fc93d7d9c24d2ceb08419ebc
tykatyk <tykatyk@gmail.com>
2022-01-03 21:59:59 +0200

    Refactor UI theme
    Add localization

--- client/uiParts/themeStub.js
+++ client/uiParts/themeStub.js
@@ -1,16 +1,23 @@
 import { createMuiTheme } from '@material-ui/core/styles'
-export const theme = createMuiTheme({
-  palette: {
-    type: 'dark',
-    primary: {
-      main: '#1a2b34', // dark blue-green
-    },
-    secondary: {
-      main: '#f8bc45', // yellow
-    },
-    background: {
-      paper: '#37474f', //blue-grey
-      default: '#223c4a', // blue-green
+import { ruRU as coreRuRu } from '@material-ui/core/locale'
+import { ruRU } from '@mui/x-data-grid'
+
+export const theme = createMuiTheme(
+  {
+    palette: {
+      type: 'dark',
+      primary: {
+        main: '#1a2b34', // dark blue-green
+      },
+      secondary: {
+        main: '#f8bc45', // yellow
+      },
+      background: {
+        paper: '#37474f', //blue-grey
+        default: '#223c4a', // blue-green
+      },
     },
   },
-})
+  coreRuRu,
+  ruRU
+)
