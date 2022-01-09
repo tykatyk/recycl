4f8755d1013d2aadabcfd7adec9b9743e6b5fbf2
tykatyk <tykatyk@gmail.com>
2022-01-09 21:47:33 +0200

    Refactor removal applications page
    to use Map component

--- client/ApplicationsPage.jsx
+++ client/ApplicationsPage.jsx
@@ -1,75 +1,26 @@
-import React, { useRef, useEffect, useState } from 'react'
-import { Wrapper, Status } from '@googlemaps/react-wrapper'
-import Snackbar from './uiParts/Snackbars.jsx'
+import React, { useEffect, useState } from 'react'
 import MapLayout from './layouts/MapLayout.jsx'
-import PageLoadingCircle from './uiParts/PageLoadingCircle.jsx'
-
-const render = (status) => {
-  if (status === Status.LOADING) return <PageLoadingCircle />
-
-  if (status === Status.FAILURE) {
-    return (
-      <Snackbar
-        severity="error"
-        open={true}
-        message="Не могу загрузить карту"
-      />
-    )
-  }
-  return null
-}
-
-const ApplicationsMap = (props) => {
-  const { center, zoom } = props
-  const ref = useRef()
-  useEffect(() => {
-    new window.google.maps.Map(ref.current, {
-      center,
-      zoom,
-    })
-  })
-
-  return (
-    <div
-      style={{
-        flex: '1 1 auto',
-        width: '100%',
-      }}
-      ref={ref}
-      id="map"
-    />
-  )
-}
+import Map from './uiParts/Map.jsx'
+import getCoordinates from '../lib/getCoordinates'
 
 export default function ApplicationsPage() {
-  const zoom = 11
   const [center, setCenter] = useState(null)
+  const [zoom, setZoom] = useState(5)
+
   useEffect(() => {
-    if (typeof window != 'undefined' && 'geolocation' in navigator) {
-      navigator.geolocation.getCurrentPosition(
-        (position) => {
-          const centerPos = {}
-          centerPos.lat = position.coords.latitude
-          centerPos.lng = position.coords.longitude
-          setCenter(centerPos)
-        },
-        (error) => {
-          console.log('error is')
-          console.log(error)
-        }
-      )
-    } else if (typeof window != 'undefined' && !('geolocation' in navigator)) {
-      console.log('Geolocation is not supported')
-    }
+    getCoordinates()
+      .then((coordinates) => {
+        setCenter(coordinates)
+        setZoom(11)
+      })
+      .catch((error) => {
+        setCenter({ lat: 49.82, lng: 21.01 })
+      })
   }, [])
+
   return (
     <MapLayout title="Recycl | Сбор отходов">
-      <Wrapper
-        apiKey={process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY}
-        render={render}
-      >
-        <ApplicationsMap center={center} zoom={zoom} />
-      </Wrapper>
+      <Map center={center} zoom={zoom} />
     </MapLayout>
   )
 }
