baff12f3b961e59c83236f147eb9ebdfa380a364
tykatyk <tykatyk@gmail.com>
2022-01-10 10:33:36 +0200

    Refactor map sidebar to use separate comonents
    instead of placing all logic into one component

--- client/uiParts/MapSidebar.jsx
+++ client/uiParts/MapSidebar.jsx
@@ -1,27 +1,28 @@
 import React, { useState } from 'react'
 import clsx from 'clsx'
-import { Tooltip, IconButton, makeStyles } from '@material-ui/core'
-import Drawer from '@material-ui/core/Drawer'
-import Toolbar from '@material-ui/core/Toolbar'
-import List from '@material-ui/core/List'
-import Divider from '@material-ui/core/Divider'
-import ListItem from '@material-ui/core/ListItem'
-import ListItemIcon from '@material-ui/core/ListItemIcon'
-import ListItemText from '@material-ui/core/ListItemText'
-import ChevronLeftIcon from '@material-ui/icons/ChevronLeft'
-import ChevronRightIcon from '@material-ui/icons/ChevronRight'
-import InboxIcon from '@material-ui/icons/MoveToInbox'
-import MailIcon from '@material-ui/icons/Mail'
+import {
+  Drawer,
+  Toolbar,
+  List,
+  ListItem,
+  ListItemIcon,
+  ListItemText,
+  Divider,
+  makeStyles,
+} from '@material-ui/core'
+import HomeIcon from '@material-ui/icons/Home'
+import BlurOnIcon from '@material-ui/icons/BlurOn'
+import LineWeightIcon from '@material-ui/icons/LineWeight'
+import ExpandLess from '@material-ui/icons/ExpandLess'
+import ExpandMore from '@material-ui/icons/ExpandMore'
+import AddIcon from '@material-ui/icons/Add'
+import MapSidebarQuantity from './MapSidebarQuantity'
+import MapActionButton from './MapActionButton'
+import MapSidebarWasteTypes from './MapSidebarWasteTypes'
 
 const drawerWidth = 240
 
 const useStyles = makeStyles((theme) => ({
-  root: {
-    display: 'flex',
-  },
-  appBar: {
-    zIndex: theme.zIndex.drawer + 1,
-  },
   drawer: {
     width: drawerWidth,
     flexShrink: 0,
@@ -40,74 +41,87 @@ const useStyles = makeStyles((theme) => ({
       duration: theme.transitions.duration.leavingScreen,
     }),
     overflowX: 'hidden',
-    width: theme.spacing(7) + 1,
-    [theme.breakpoints.up('sm')]: {
-      width: theme.spacing(9) + 1,
-    },
+    width: 0,
   },
-  content: {
-    flexGrow: 1,
-    padding: theme.spacing(3),
+  secondaryColor: {
+    color: theme.palette.secondary.main,
   },
 }))
 
 export default function MapSidebar() {
   const classes = useStyles()
   const [open, setOpen] = useState(false)
+  const [wasteTypeOpen, setWasteTypeOpen] = useState(true)
+  const [quantityOpen, setQuantityOpen] = useState(true)
+  const [checked, setChecked] = useState([0])
 
-  const handleDrawerOpen = () => {
-    setOpen(true)
+  const handleDrawerToggle = () => {
+    setOpen(!open)
   }
-
-  const handleDrawerClose = () => {
-    setOpen(false)
+  const handleWasteTypeToggle = () => {
+    setWasteTypeOpen(!wasteTypeOpen)
+  }
+  const handleQuantityToggle = () => {
+    setQuantityOpen(!quantityOpen)
   }
 
   return (
-    <Drawer
-      variant="permanent"
-      className={clsx(classes.drawer, {
-        [classes.drawerOpen]: open,
-        [classes.drawerClose]: !open,
-      })}
-      classes={{
-        paper: clsx({
+    <>
+      <MapActionButton onClick={handleDrawerToggle} open={open} />
+      <Drawer
+        variant="permanent"
+        className={clsx(classes.drawer, {
           [classes.drawerOpen]: open,
           [classes.drawerClose]: !open,
-        }),
-      }}
-    >
-      <Toolbar />
-      <List>
-        {open && (
-          <ListItem button onClick={handleDrawerClose} key="Скрыть панель">
+        })}
+        classes={{
+          paper: clsx({
+            [classes.drawerOpen]: open,
+            [classes.drawerClose]: !open,
+          }),
+        }}
+      >
+        <Toolbar />
+        <List>
+          <ListItem button component="a" href="/" key="На главную">
             <ListItemIcon>
-              <ChevronLeftIcon />
+              <HomeIcon />
             </ListItemIcon>
+            <ListItemText primary="На главную" />
           </ListItem>
-        )}
-        {!open && (
-          <ListItem button onClick={handleDrawerOpen} key="Открыть панель">
-            <Tooltip title="Открыть панель">
-              <ListItemIcon>
-                <ChevronRightIcon />
-              </ListItemIcon>
-            </Tooltip>
+          <ListItem
+            button
+            component="a"
+            href="removal/application/create"
+            key="Подать заявку"
+            className={classes.secondaryColor}
+          >
+            <ListItemIcon>
+              <AddIcon color="secondary" />
+            </ListItemIcon>
+            <ListItemText primary="Подать заявку" />
           </ListItem>
-        )}
-      </List>
+          <Divider variant="inset" component="li" />
+          <ListItem button key={'Количество'} onClick={handleQuantityToggle}>
+            <ListItemIcon>
+              <LineWeightIcon />
+            </ListItemIcon>
+            <ListItemText primary={'Количество'} />
+            {quantityOpen ? <ExpandLess /> : <ExpandMore />}
+          </ListItem>
+          <MapSidebarQuantity open={quantityOpen} />
+          <Divider variant="inset" component="li" />
+          <ListItem button key={'Тип отходов'} onClick={handleWasteTypeToggle}>
+            <ListItemIcon>
+              <BlurOnIcon />
+            </ListItemIcon>
+            <ListItemText primary={'Тип отходов'} />
+            {wasteTypeOpen ? <ExpandLess /> : <ExpandMore />}
+          </ListItem>
 
-      <Divider />
-      <List>
-        {['Тип отходов', 'Количество'].map((text, index) => (
-          <ListItem button key={text}>
-            <ListItemIcon>
-              {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-            </ListItemIcon>
-            <ListItemText primary={text} />
-          </ListItem>
-        ))}
-      </List>
-    </Drawer>
+          <MapSidebarWasteTypes open={wasteTypeOpen} />
+        </List>
+      </Drawer>
+    </>
   )
 }
