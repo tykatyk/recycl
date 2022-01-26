42491d3bb1fabdf8f9ad2145ba79480bca158880
tykatyk <tykatyk@gmail.com>
2022-01-26 21:22:37 +0200

    Add site logo

---
+++ client/uiParts/header/Logo.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import { makeStyles } from '@material-ui/core'
+import Link from '../Link.jsx'
+const useStyles = makeStyles({
+  root: {
+    fontSize: '1.5rem',
+    fontFamily: 'inherit',
+    fontWeight: 'bold',
+    lineHeight: '1.334',
+    letterSpacing: '0em',
+    color: '#adce5d',
+  },
+})
+
+export default function Logo() {
+  const classes = useStyles()
+  return (
+    <Link underline="none" href="/" className={classes.root}>
+      recycl
+    </Link>
+  )
+}
