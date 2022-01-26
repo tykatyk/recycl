fe3dc2a842382b9c61ef33ee840304c07b6b79e5
tykatyk <tykatyk@gmail.com>
2022-01-26 21:32:45 +0200

    Rearrange wrapper children

--- client/uiParts/Wrapper.jsx
+++ client/uiParts/Wrapper.jsx
@@ -4,7 +4,6 @@ import { makeStyles } from '@material-ui/core/styles'
 const useStyles = makeStyles((theme) => ({
   wrapper: {
     display: 'flex',
-    flexWrap: 'wrap',
     boxSizing: 'border-box',
     flexDirection: 'column',
     minHeight: '100vh',
