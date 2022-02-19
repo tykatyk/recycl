5492f01a543e115030885c8d367d74dca073e0f1
tykatyk <tykatyk@gmail.com>
2022-02-19 07:58:23 +0200

    Fix not updating info window content

--- client/uiParts/Marker.jsx
+++ client/uiParts/Marker.jsx
@@ -1,11 +1,12 @@
 import React, { useState, useEffect } from 'react'
-import ReactDOMServer from 'react-dom/server'
+import ReactDOM from 'react-dom'
 
 export default function Marker(options) {
   const [marker, setMarker] = useState(null)
   const { children, map, ...rest } = options
   const [infoWindow, setInfoWindow] = useState(null)
   const [listenerAdded, setListenerAdded] = useState(false)
+  const [content, setContent] = useState(document.createElement('div'))
 
   useEffect(() => {
     if (!marker) {
@@ -36,16 +37,17 @@ export default function Marker(options) {
     }
     if (infoWindow && !listenerAdded) {
       marker.addListener('click', () => {
-        
         infoWindow.open({
           anchor: marker,
           map,
           shouldFocus: false,
         })
       })
+
+      infoWindow.setContent(content)
       setListenerAdded(true)
     }
   }, [marker, rest])
 
-  return null
+  return ReactDOM.createPortal(children, content)
 }
