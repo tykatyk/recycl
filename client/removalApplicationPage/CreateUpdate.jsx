39842a7551bd2b19fca317667e98368dd2b27f56
tykatyk <tykatyk@gmail.com>
2022-02-21 06:54:47 +0200

    Add redirection of unathenticated user

--- client/removalApplicationPage/CreateUpdate.jsx
+++ client/removalApplicationPage/CreateUpdate.jsx
@@ -1,22 +1,25 @@
 import React from 'react'
-import Grid from '@material-ui/core/Grid'
+import { Grid } from '@material-ui/core'
 import Layout from '../layouts/Layout.jsx'
 import RemovalForm from './RemovalForm.jsx'
+import RedirectUnathenticatedUser from '../uiParts/RedirectUnathenticatedUser.jsx'
 
 export default function RemovalApplication(props) {
   return (
-    <Layout title="Recycl | Removal Application">
-      <Grid
-        container
-        direction="column"
-        style={{
-          maxWidth: '750px',
-          margin: '0 auto',
-          padding: '16px',
-        }}
-      >
-        <RemovalForm {...props} />
-      </Grid>
-    </Layout>
+    <RedirectUnathenticatedUser>
+      <Layout title="Recycl | Removal Application">
+        <Grid
+          container
+          direction="column"
+          style={{
+            maxWidth: '750px',
+            margin: '0 auto',
+            padding: '16px',
+          }}
+        >
+          <RemovalForm {...props} />
+        </Grid>
+      </Layout>
+    </RedirectUnathenticatedUser>
   )
 }
