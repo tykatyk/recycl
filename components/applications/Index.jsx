4625a11b5c7f57a34feb24b43747301700f9b37a
tykatyk <tykatyk@gmail.com>
2022-03-01 20:53:22 +0200

    Add showing applications by city and waste type

--- components/applications/Index.jsx
+++ components/applications/Index.jsx
@@ -2,11 +2,9 @@ import React, { useState } from 'react'
 import {
   Grid,
   Typography,
-  Badge,
   makeStyles,
   TablePagination,
 } from '@material-ui/core'
-import MailIcon from '@material-ui/icons/Mail'
 import Layout from '../layouts/Layout.jsx'
 import Snackbar from '../uiParts/Snackbars.jsx'
 import PageLoadingCircle from '../uiParts/PageLoadingCircle.jsx'
@@ -14,12 +12,8 @@ import DataGridFooter from '../uiParts/DataGridFooter.jsx'
 import DataGridNoRowsOverlay from '../uiParts/DataGridNoRowsOverlay.jsx'
 import DataGridErrorOverlay from '../uiParts/DataGridErrorOverlay.jsx'
 import { DataGrid } from '@mui/x-data-grid'
-import { useQuery, useMutation } from '@apollo/client'
-import { useSession } from 'next-auth/react'
-import {
-  GET_REMOVAL_APPLICATIONS_WITH_MESSAGE_COUNT,
-  DELETE_REMOVAL_APPLICATIONS,
-} from '../../lib/graphql/queries/removalApplication'
+import { useQuery } from '@apollo/client'
+import { GET_REMOVAL_APPLICATIONS } from '../../lib/graphql/queries/removalApplication'
 import Router from 'next/router'
 
 const useStyles = makeStyles((theme) => ({
@@ -54,48 +48,19 @@ const columns = [
     align: 'center',
   },
   {
-    field: 'messageCount',
-    headerName: 'Сообщения',
-    width: 150,
-    headerAlign: 'center',
-    align: 'center',
-    renderCell: (params) => {
-      if (params.row.messageCount > 0) {
-        return (
-          <Badge badgeContent={params.row.messageCount} color="secondary">
-            <MailIcon />
-          </Badge>
-        )
-      }
-      return ''
-    },
-  },
-  {
     field: 'expires',
     headerName: 'До',
     width: 110,
   },
 ]
 
-export default function removalApplications(props) {
+export default function Index(props) {
   const classes = useStyles()
-  const [selected, setSelected] = useState([])
   const [page, setPage] = useState(0)
   const [pageSize, setPageSize] = useState(10)
   const [backendError, setBackendError] = useState(null)
-  const { status } = useSession()
-  const { loading, error, data } = useQuery(
-    GET_REMOVAL_APPLICATIONS_WITH_MESSAGE_COUNT
-  )
-  const [deleteMutation, { loading: deleting, error: deletionError }] =
-    useMutation(DELETE_REMOVAL_APPLICATIONS)
-  const deleteHandler = (event) => {
-    if (selected.length < 1) return
-    deleteMutation({
-      variables: { ids: selected },
-      refetchQueries: [{ query: GET_REMOVAL_APPLICATIONS_WITH_MESSAGE_COUNT }],
-    })
-  }
+  const { loading, error, data } = useQuery(GET_REMOVAL_APPLICATIONS)
+
   const handlePageChange = (_, newPage) => setPage(newPage)
   const handlePageSizeChange = (event) => {
     setPageSize(parseInt(event.target.value, 10))
@@ -105,109 +70,83 @@ export default function removalApplications(props) {
 
   if (loading) return <PageLoadingCircle />
 
-  if (status === 'unauthenticated') {
-    Router.push({
-      pathname: '/auth/login',
-      query: {
-        from: '/removal/application',
-      },
+  if (!error && data.getRemovalApplications) {
+    //ToDo: Refactor returned data, so that data contain objects in needed form
+    //so that to avoid mapping
+    rows = data.getRemovalApplications.map((item) => {
+      const newItem = {}
+      newItem.id = item.document['_id']
+      newItem.wasteType = item.document.wasteType.name
+      newItem.wasteLocation = item.document.wasteLocation.description
+      newItem.quantity = item.document.quantity
+      newItem.messageCount = item.messageCount
+      const expires = new Date()
+      newItem.expires = expires.toLocaleString('ru-RU', {
+        day: '2-digit',
+        month: 'short',
+      })
+      return newItem
     })
   }
 
-  if (status === 'authenticated') {
-    if (!error) {
-      //ToDo: Refactor returned data, so that data contain objects in needed form
-      //so that to avoid mapping
-      rows = data.getRemovalApplicationsWithMessageCount.map((item) => {
-        const newItem = {}
-        newItem.id = item.document['_id']
-        newItem.wasteType = item.document.wasteType.name
-        newItem.wasteLocation = item.document.wasteLocation.description
-        newItem.quantity = item.document.quantity
-        newItem.messageCount = item.messageCount
-        const expires = new Date()
-        newItem.expires = expires.toLocaleString('ru-RU', {
-          day: '2-digit',
-          month: 'short',
-        })
-        return newItem
-      })
-    }
-
-    return (
-      <>
-        <Layout title="Мои заявки на вывоз | Recycl">
-          <Grid
-            container
-            direction="column"
-            style={{
-              margin: '0 auto',
-              padding: '16px',
-            }}
-          >
-            <Typography gutterBottom variant="h4">
-              Заявки на вывоз отходов
-            </Typography>
-            <div style={{ width: '100%' }}>
-              <DataGrid
-                classes={{ root: classes.root, row: classes.row }}
-                autoHeight
-                error={error}
-                rows={rows}
-                columns={columns}
-                pagination
-                page={page}
-                pageSize={pageSize}
-                checkboxSelection
-                disableSelectionOnClick
-                onPageChange={handlePageChange}
-                onPageSizeChange={handlePageSizeChange}
-                onCellClick={(params, event) => {
-                  if (params.field === 'messages') {
-                    Router.push(`/messages`)
-                    return
-                  }
-                  if (params.field !== '__check__')
-                    Router.push(`/removal/application/${params.id}`)
-                }}
-                onSelectionModelChange={(params) => {
-                  setSelected(params)
-                }}
-                components={{
-                  Footer: (props) => {
-                    return <DataGridFooter {...props} />
-                  },
-                  Pagination: TablePagination,
-                  NoRowsOverlay: DataGridNoRowsOverlay,
-                  ErrorOverlay: DataGridErrorOverlay,
-                }}
-                componentsProps={{
-                  footer: {
-                    deleteHandler,
-                    deleting,
-                    selected,
-                    numRows: rows && rows.length ? rows.length : 0,
-                    handlePageChange,
-                    handlePageSizeChange,
-                    pageSize,
-                    page,
-                  },
-                  checkbox: { color: 'secondary' },
-                }}
-              />
-            </div>
-          </Grid>
-        </Layout>
-        <Snackbar
-          severity="error"
-          open={!!backendError}
-          message={backendError}
-          handleClose={() => {
-            setBackendError(null)
+  return (
+    <>
+      <Layout title="Заявки на вывоз | Recycl">
+        <Grid
+          container
+          direction="column"
+          style={{
+            margin: '0 auto',
+            padding: '16px',
           }}
-        />
-      </>
-    )
-  }
-  return ''
+        >
+          <Typography gutterBottom variant="h4">
+            Заявки на вывоз отходов
+          </Typography>
+          <div style={{ width: '100%' }}>
+            <DataGrid
+              classes={{ root: classes.root, row: classes.row }}
+              autoHeight
+              error={error}
+              rows={rows}
+              columns={columns}
+              pagination
+              page={page}
+              pageSize={pageSize}
+              onPageChange={handlePageChange}
+              onPageSizeChange={handlePageSizeChange}
+              onCellClick={(params, event) => {
+                Router.push(`/applications/${params.id}`)
+              }}
+              components={{
+                Footer: (props) => {
+                  return <DataGridFooter {...props} />
+                },
+                Pagination: TablePagination,
+                NoRowsOverlay: DataGridNoRowsOverlay,
+                ErrorOverlay: DataGridErrorOverlay,
+              }}
+              componentsProps={{
+                footer: {
+                  numRows: rows.length,
+                  handlePageChange,
+                  handlePageSizeChange,
+                  pageSize,
+                  page,
+                },
+              }}
+            />
+          </div>
+        </Grid>
+      </Layout>
+      <Snackbar
+        severity="error"
+        open={!!backendError}
+        message={backendError}
+        handleClose={() => {
+          setBackendError(null)
+        }}
+      />
+    </>
+  )
 }
