c32635de4a52ebd66ee5e9b900fc4c87db9545b9
tykatyk <tykatyk@gmail.com>
2022-03-25 19:50:59 +0200

    Add backend logic for contactUs page

---
+++ pages/api/contactus.js
@@ -0,0 +1,32 @@
+import { contactUsSchema } from '../../lib/validation'
+import { checkCaptcha } from '../../lib/checkCaptcha'
+import sendEmail from '../../lib/sendEmail'
+import {
+  errorResponse,
+  captchaNotPassedResponse,
+} from '../../lib/helpers/responses'
+
+export default async function contactUsHandler(req, res) {
+  const { recaptcha, ...messageData } = req.body
+  const captchaPassed = await checkCaptcha(recaptcha)
+  if (!captchaPassed) return captchaNotPassedResponse(res)
+
+  try {
+    await contactUsSchema.validate(messageData, { abortEarly: false })
+  } catch (error) {
+    console.log(error)
+    return errorResponse(error, res)
+  }
+
+  const contactUsEmail = 'tykatyk@gmail.com'
+  const frontendMessage = 'Сообщение отправлено'
+  const message = `Новое сообщение от пользователя ${messageData.username}.\r\n Электронная почта ${messageData.email}. \r\n ${messageData.message}`
+  return await sendEmail(res, {
+    to: contactUsEmail,
+    from: messageData.email,
+    name: 'Recycl contact page',
+    subject: messageData.subject,
+    message,
+    frontendMessage,
+  })
+}
