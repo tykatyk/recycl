924ee0d8bb11bba564332ac7858801d480d2d824
tykatyk <tykatyk@gmail.com>
2022-03-27 21:00:28 +0300

    Start adding logic to delete stale dialogs

--- pages/my/dialogs/index.js
+++ pages/my/dialogs/index.js
@@ -1,6 +1,32 @@
 import React from 'react'
 import Dialogs from '../../../components/dialogs/DialogsPage.jsx'
+import { DELETE_STALE_DIALOGS } from '../../../lib/graphql/queries/message'
+import appoloClient from '../../../lib/appoloClient/appoloClient'
+import { getSession } from 'next-auth/react'
 
 export default function Index() {
   return <Dialogs />
+}
+
+export async function getServerSideProps(context) {
+  const res = { props: {} }
+
+  await appoloClient
+    .mutate({
+      mutation: DELETE_STALE_DIALOGS,
+      context: {
+        headers: {
+          cookie: context.req.headers.cookie,
+        },
+      },
+    })
+    .then((result) => {
+      // console.log('result is')
+      // console.log(result)
+    })
+    .catch((error) => {
+      // console.log('error in indexjs')
+      // console.log(JSON.stringify(error, null, 2))
+    })
+  return res
 }
