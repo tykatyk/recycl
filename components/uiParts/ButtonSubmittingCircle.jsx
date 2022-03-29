0ea90edd2439c0d4656010e115f5f6760eea8ccb
tykatyk <tykatyk@gmail.com>
2022-03-29 12:45:22 +0300

    Refactor default color setting

--- components/uiParts/ButtonSubmittingCircle.jsx
+++ components/uiParts/ButtonSubmittingCircle.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
-import { CircularProgress, useTheme } from '@material-ui/core'
+import { CircularProgress } from '@material-ui/core'
 
-export default function ButtonSubmittingCircle() {
-  const theme = useTheme()
+export default function ButtonSubmittingCircle(props) {
+  const { color = 'secondary' } = props
+
   return (
     <CircularProgress
       size={24}
+      color={color}
       style={{
-        color: theme.palette.secondary.main,
         marginLeft: '1em',
       }}
     />
