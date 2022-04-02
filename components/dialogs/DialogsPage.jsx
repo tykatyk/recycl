ada959aa21f74c5b1f47d5bfe5f224b334df7261
tykatyk <tykatyk@gmail.com>
2022-04-02 12:01:29 +0300

    Fix accessing non-existing property

--- components/dialogs/DialogsPage.jsx
+++ components/dialogs/DialogsPage.jsx
@@ -150,7 +150,7 @@ export default function DialogsPage() {
 
       if (
         !result ||
-        !result.some ||
+        !result.data ||
         !result.data.deleteDialogs ||
         !result.data.deleteDialogs.deletedCount
       ) {
