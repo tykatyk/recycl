7ed6e7714fd979f52efaa7aa4bda2c0db5d4c092
tykatyk <tykatyk@gmail.com>
2022-04-02 11:48:41 +0300

    Fix incorrect path

--- pages/my/account/index.js
+++ pages/my/account/index.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import AllSettings from '../../../client/uiParts/userSettings/AllSettings.jsx'
+import AllSettings from '../../../components/uiParts/userSettings/AllSettings.jsx'
 
 export default function userSettings() {
   return <AllSettings />
