eb19698b10be1d6b38fa1565664029b0a95c1c09
tykatyk <tykatyk@gmail.com>
2022-04-26 22:02:51 +0300

    Refactor render logic
    Now rendering all data at once

--- components/ChatPage.jsx
+++ components/ChatPage.jsx
@@ -30,7 +30,6 @@ const useStyles = makeStyles({
   dialog: {
     height: messageContainerHeight,
     overflowY: 'auto',
-    position: 'relative',
   },
 
   right: {
@@ -45,12 +44,6 @@ const useStyles = makeStyles({
     minHeight: 450,
   },
   fromMe: { alignItems: 'flex-end' },
-  runway: {
-    position: 'absolute',
-    width: 1,
-    height: 1,
-    padding: 0,
-  },
 })
 
 export default function ChatPage(props) {
@@ -76,130 +69,52 @@ export default function ChatPage(props) {
     return false
   }
 
-  const numItemsToRenderForward = 1
-  const numItemsToRenderBackward = 1
   const limit = 1 //num messages to receive when quering database
   const messageContainerRef = useRef()
-  const runwayRef = useRef()
   const nodesRef = useRef([])
   const [initialLoad, setInitialLoad] = useState(true)
-  const items = useRef([])
-  const [runwayHeight, setRunwayHeight] = useState(0)
-  const [startIndex, setStartIndex] = useState(0)
-  const [technicalScroll, setTechnicalScroll] = useState(false)
-  const [endIndex, setEndIndex] = useState(-1)
-  const [anchorEl, setAnchorEl] = useState({
-    index: 0,
-    //ToDo: maybe rename
-    offset: 0, //num pixels of anchor element which are above visible area of message container
-    scrollTop: 0, //current scroll position
-  })
+  const [items, setItems] = useState([])
+  const [anchorIndex, setAnchorIndex] = useState(0)
   const [canLoadMore, setCanLoadMore] = useState(true)
-  //load initial dialog data
+
   useEffect(() => {
-    if (!dialogId) return
     getMoreData()
-  }, [dialogId])
+  }, [])
 
-  //This runs after all dom manipulations
-  //Position all rendered elements
-  //and measure new height of runway
-  // useEffect(() => {
-  useLayoutEffect(() => {
-    if (!items.current || items.current.length === 0) return
+  useEffect(() => {
+    if (!messageContainerRef.current) return
+    let nodeHeight = 0
+    let i = 0
+    let currentPos = 0
 
-    let i
-    //position from the top of the message container
-    let currentPos
-    let newRunwayHeight = runwayHeight
-    let node
-    let nodeHeight
-    console.log('f ' + startIndex)
-    console.log('l ' + endIndex)
-
-    if (!items.current[endIndex].height) {
-      i = endIndex
-      currentPos = 0
-      let newScroll = 0
-
-      while (i >= startIndex) {
-        node = nodesRef.current[i]
-        nodeHeight = nodesRef.current[i].offsetHeight
-        //ToDo maybe add animation when positioning elements
-        node.style.top = `${currentPos}px`
-        if (i > anchorEl.index) {
-          newScroll += nodeHeight
-        }
-
-        if (!items.current[i].height) {
-          items.current[i].height = nodeHeight
-          newRunwayHeight += items.current[i].height
-        }
-        currentPos += nodeHeight
-        i--
-      }
-      messageContainerRef.current.scrollTop = newScroll
-      setTechnicalScroll(true)
-    } else {
-      console.log('here')
-      i = anchorEl.index
-
-      currentPos = anchorEl.scrollTop - anchorEl.offset
-
-      while (i >= startIndex) {
-        node = nodesRef.current[i]
-        nodeHeight = nodesRef.current[i].offsetHeight
-        //ToDo maybe add animation when positioning elements
-        node.style.top = `${currentPos}px`
-        if (i <= anchorEl.index) currentPos += nodeHeight
-
-        if (!items.current[i].height) {
-          items.current[i].height = nodeHeight
-          newRunwayHeight += items.current[i].height
-        }
-        i--
-      }
-
-      i = anchorEl.index
-      currentPos = nodesRef.current[anchorEl.index].offsetHeight
-
-      while (i < endIndex) {
-        node = nodesRef.current[i + 1]
-        nodeHeight = nodesRef.current[i + 1].offsetHeight
-        //ToDo maybe add animation when positioning elements
-        node.style.top = `${currentPos - nodeHeight}px`
-        currentPos -= nodeHeight
-
-        if (!items.current[i].height) {
-          items.current[i].height = nodeHeight
-          newRunwayHeight += items.current[i].height
-        }
-        i++
-      }
-      messageContainerRef.current.scrollTop = anchorEl.scrollTop
+    while (i < anchorIndex) {
+      nodeHeight = nodesRef.current[i].offsetHeight
+      currentPos += nodeHeight
+      i++
     }
-
-    setRunwayHeight(newRunwayHeight)
     if (initialLoad) {
       messageContainerRef.current.scrollTop =
         messageContainerRef.current.scrollHeight
+    } else {
+      messageContainerRef.current.scrollTop = currentPos
     }
 
+    console.log('index ' + anchorIndex)
     if (
       messageContainerRef.current.scrollHeight <=
         messageContainerRef.current.offsetHeight &&
       canLoadMore
     ) {
-      getMoreData(items.current[items.current.length - 1].data._id)
+      getMoreData(items[0].data._id)
     } else {
       setInitialLoad(false)
     }
-  }, [startIndex, endIndex])
+  }, [items, anchorIndex])
 
   const handleClick = (e) => {
-    if (!dialogId || items.current.length == 0) return //ToDo: handle errror
+    if (!dialogId || items.length == 0) return //ToDo: handle errror
 
-    const firstMessage = items.current[0].data
+    const firstMessage = items[0].data
     //dialogReceiverId and dialogInitiatorId are the same in each message which belongs to this dialog
     let dialogReceiverId = firstMessage.dialogReceiverId
     let dialogInitiatorId = firstMessage.dialogInitiatorId
@@ -263,123 +178,14 @@ export default function ChatPage(props) {
 
   //ToDo: scroll to the end of message container on initial load
   const handleScroll = async (e) => {
-    if (typeof window === 'undefined' || loading) return
-
-    const currScroll = messageContainerRef.current
-      ? messageContainerRef.current.scrollTop
-      : 0
-    if (technicalScroll) {
-      setAnchorEl(calculateAnchoredItem())
-      setTechnicalScroll(false)
+    if (messageContainerRef.current.scrollTop === 0 && canLoadMore) {
+      getMoreData(items[0].data._id)
       return
     }
-    let prevScroll = anchorEl.scrollTop
-    let anchorIndex = anchorEl.index
-    let delta = currScroll - prevScroll
-    const newAnchorEl = calculateAnchoredItem()
-    //we are at the top of the dialog and want to load more messages
-    if (
-      messageContainerRef.current.scrollTop == 0 && //ToDo potential problem
-      canLoadMore
-    ) {
-      setAnchorEl(newAnchorEl)
-
-      //offset in items to load
-      const lastItemId = items.current.length
-        ? items.current[items.current.length - 1].data._id
-        : ''
-      const result = await getMoreData(lastItemId)
-      let numLoaded = result ? result.data.getDialog.length : 0
-
-      fill(
-        anchorIndex - numItemsToRenderBackward,
-        anchorIndex + numItemsToRenderForward + numLoaded
-      )
-      return
-    }
-
-    //position of top edje of the anchor relative to current scroll position
-    // console.log('updated delta ' + delta)
-    //scroll down
-    if (delta > 0) {
-      fill(
-        anchorIndex - numItemsToRenderBackward,
-        anchorIndex + numItemsToRenderForward
-      )
-    } else {
-      //scroll up
-      console.log('up')
-
-      fill(
-        anchorIndex - numItemsToRenderForward,
-        anchorIndex + numItemsToRenderBackward
-      )
-    }
-    console.log('a ' + anchorEl.index)
-    setAnchorEl(newAnchorEl)
-  }
-  const calculateAnchoredItem = () => {
-    const currScroll = messageContainerRef.current
-      ? messageContainerRef.current.scrollTop
-      : 0
-    if (technicalScroll) {
-      return {
-        index: anchorEl.index,
-        offset: anchorEl.offset,
-        scrollTop: currScroll,
-      }
-    }
-    let prevScroll = anchorEl.scrollTop
-    let anchorIndex = anchorEl.index
-    let delta = currScroll - prevScroll
-
-    //we are at the top of the dialog and want to load more messages
-    if (
-      messageContainerRef.current.scrollTop == 0 && //ToDo potential problem
-      canLoadMore
-    ) {
-      anchorIndex = items.current.length > 0 ? items.current.length - 1 : 0
-      return {
-        index: anchorIndex,
-        offset: 0,
-        scrollTop: 0,
-      }
-    }
-
-    delta += anchorEl.offset
-    console.log('delta ' + delta)
-
-    //calculate position of top edje of the anchor relative to current scroll position
-    if (delta > 0) {
-      while (
-        anchorIndex > 0 &&
-        delta > 0 &&
-        delta > items.current[anchorIndex].height
-      ) {
-        delta -= items.current[anchorIndex].height
-        anchorIndex--
-      }
-    } else {
-      while (anchorIndex < items.current.length - 1 && delta < 0) {
-        delta += items.current[anchorIndex + 1].height
-        anchorIndex++
-      }
-    }
-
-    return {
-      index: anchorIndex,
-      offset: delta,
-      scrollTop: currScroll,
-    }
-  }
-
-  const fill = (start, end) => {
-    setStartIndex(Math.max(start, 0))
-    setEndIndex(Math.min(items.current.length - 1, end))
   }
 
   const getMoreData = async function (offset = '', count = limit) {
-    if (!canLoadMore) return
+    if (!dialogId || !canLoadMore) return
     setLoading(true)
     return client
       .query({
@@ -393,13 +199,16 @@ export default function ChatPage(props) {
       .then((result) => {
         if (!dataIsCorrect(result.data)) return
 
-        const newItems = [...items.current]
+        const prevItems = items
+        let newItems = []
         result.data.getDialog.forEach((item) =>
           newItems.push({ data: item, height: 0 })
         )
-        items.current = newItems
+        setItems([...newItems, ...prevItems])
 
-        if (initialLoad) setEndIndex(result.data.getDialog.length - 1)
+        const numLoaded = result.data.getDialog.length
+        setAnchorIndex(numLoaded)
+
         if (canLoadMore && result.data.getDialog.length < count)
           setCanLoadMore(false)
 
@@ -410,7 +219,6 @@ export default function ChatPage(props) {
         setGetDialogError(true)
         console.log('error in loading data')
         // console.log(JSON.stringify(error, null, 2))
-        // return 0
         return null
         //ToDo handle error
       })
@@ -419,18 +227,9 @@ export default function ChatPage(props) {
   let content = null
   if (loading) content = <PageLoadingCircle />
   if (getDialogError) content = <ErrorOverlay /> //ToDo: Add incorrect data error
-  if (items.current && items.current.length > 0) {
-    let i = endIndex
-    const itemsToRender = []
-    // console.log('items')
-    // console.log(items)
-    while (i >= startIndex) {
-      itemsToRender.push(items.current[i])
-      i--
-    }
+  if (items.length > 0) {
     // console.log('e ' + endIndex)
     // console.log('s ' + startIndex)
-    // console.log(items.current)
 
     content = (
       <Grid container component={Paper} className={classes.chatSection}>
@@ -440,15 +239,9 @@ export default function ChatPage(props) {
             ref={messageContainerRef}
             onScroll={(e) => handleScroll(e)}
           >
-            <ListItem
-              key="runway"
-              className={classes.runway}
-              style={{ transform: `translateY(${runwayHeight}px)` }}
-              ref={runwayRef}
-            />
-            {itemsToRender.map((item, index) => {
+            {items.map((item, index) => {
               const message = item.data
-              const refIndex = endIndex - index
+              const refIndex = index
               const now = new Date()
               const currMonth = now.getMonth()
               const currDate = now.getDate()
@@ -472,12 +265,13 @@ export default function ChatPage(props) {
               }
               return (
                 <ListItem
-                  ref={(el) => (nodesRef.current[refIndex] = el)}
+                  ref={(el) => {
+                    nodesRef.current[index] = el
+                  }}
                   key={message._id}
                   className={clsx({
                     [classes.right]: message.senderId !== thisUserId,
                   })}
-                  style={{ position: 'absolute' }}
                 >
                   <Grid
                     container
