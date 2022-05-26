9b26a763857920d475868a1bbf21d09bdfcbe1fc
tykatyk <tykatyk@gmail.com>
2022-05-26 21:51:21 +0300

    Add route for socketio server initialization

---
+++ pages/api/socketio.js
@@ -0,0 +1,37 @@
+import { Server } from 'socket.io'
+
+export default async function socketHandler(req, res) {
+  if (res.socket.server.io) {
+    console.log('Socket server is already running')
+  } else {
+    console.log('Socket server is initializing')
+    const io = new Server(res.socket.server)
+
+    io.use(async (socket, next) => {
+      socket.userId = socket.handshake.auth.userId
+      next()
+    })
+
+    res.socket.server.io = io
+
+    io.on('connection', (socket) => {
+      socket.join(socket.userId)
+
+      socket.on('messageAdded', (msg) => {
+        io.to(msg.createMessage.senderId)
+          .to(msg.createMessage.receiverId)
+          .emit('newMessage', msg)
+      })
+
+      socket.on('userIsTyping', ({ receiverId }) => {
+        socket.to(receiverId).emit('typing')
+      })
+
+      socket.on('disconnect', () => {
+        console.log(`client socket disconnected`)
+      })
+    })
+  }
+
+  res.end()
+}
