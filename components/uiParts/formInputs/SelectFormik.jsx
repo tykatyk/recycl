73d8be7aa65dc4057699583391e5950bef28e24e
tykatyk <tykatyk@gmail.com>
2023-09-08 16:18:42 +0300

    Fix incorrect prop

--- components/uiParts/formInputs/SelectFormik.jsx
+++ components/uiParts/formInputs/SelectFormik.jsx
@@ -28,7 +28,6 @@ export default function SelectFormik(props) {
             vertical: 'top',
             horizontal: 'left',
           },
-          getContentAnchorEl: null,
         },
       }}
     >
