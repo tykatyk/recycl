0179859eaf482b1a3c42b63ce35d6ce9f610451e
tykatyk <tykatyk@gmail.com>
2023-09-21 15:47:20 +0300

    Add api handler to create events

---
+++ lib/db/queries/event.ts
@@ -0,0 +1,83 @@
+import getCoords from '../../helpers/getCoords'
+//import removalApplication from '../../validation/removalApplication' //ToDo: Add validation before create and update operations
+import { Event } from '../models/index'
+import { internalServerError } from '../../errors/index'
+import mongoose from 'mongoose'
+
+const eventQueries = {
+  create: async (data, user) => {
+    if (!user) return null
+
+    data.user = {}
+
+    try {
+      data.user._id = user['_id']
+      data.user.name = user.name
+
+      const event = new Event(data)
+      return await event.save()
+    } catch (error) {
+      console.log(error)
+      throw internalServerError
+    }
+  },
+
+  get: async (id) => {
+    try {
+      const result = await Event.findById(id)
+        .populate('wasteType')
+        .populate('user')
+        .exec()
+      return result
+    } catch (error) {
+      console.log(error)
+      throw internalServerError
+    }
+  },
+
+  getAll: async (queryParams, user) => {
+    try {
+      if (!user) return null
+      const query = {}
+
+      if (queryParams.wasteType)
+        query['wasteType'] = new mongoose.Types.ObjectId(queryParams.wasteType)
+      if (queryParams.city) query['wasteLocation.place_id'] = queryParams.city
+
+      return await Event.find(query).populate('wasteType')
+    } catch (error) {
+      console.log(error)
+      throw internalServerError
+    }
+  },
+
+  update: async (id, newValue) => {
+    try {
+      return await Event.findByIdAndUpdate(id, newValue, {
+        new: true,
+      }).exec()
+    } catch (error) {
+      console.log(error)
+      throw internalServerError
+    }
+  },
+
+  delete: async (id) => {
+    try {
+      return await Event.findByIdAndRemove(id).exec()
+    } catch (error) {
+      console.log(error)
+      throw internalServerError
+    }
+  },
+
+  deleteMany: async (ids) => {
+    try {
+      return await Event.deleteMany({ _id: { $in: ids } }).exec()
+    } catch (error) {
+      console.log(error)
+      throw internalServerError
+    }
+  },
+}
+export default eventQueries
---
+++ pages/api/events/index.ts
@@ -0,0 +1,51 @@
+import { getServerSession } from 'next-auth/next'
+import { authOptions } from '../auth/[...nextauth]'
+import { NextApiRequest, NextApiResponse } from 'next'
+import eventValidator from '../../../lib/validation/eventForm'
+import eventQueries from '../../../lib/db/queries/event'
+import {
+  errorResponse,
+  perFormErrorResponse,
+} from '../../../lib/helpers/responses'
+
+export default async function Events(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const session = await getServerSession(req, res, authOptions)
+
+  if (!session) {
+    res.status(401).end()
+    return
+  }
+
+  if (req.method === 'GET') {
+  }
+
+  if (req.method === 'POST') {
+    try {
+      await eventValidator.validate(req.body, {
+        abortEarly: false,
+      })
+    } catch (error) {
+      console.log(error)
+      return errorResponse(error, res)
+    }
+
+    try {
+      await eventQueries.create(req.body, {
+        _id: session.id,
+        name: session.user?.name,
+      })
+    } catch (e) {
+      perFormErrorResponse('Ошибка при создании документа', res)
+      return
+    }
+
+    res.status(200).json({ message: 'Event successfully created' })
+  }
+  if (req.method === 'PUT') {
+  }
+  if (req.method === 'DELETE') {
+  }
+}
