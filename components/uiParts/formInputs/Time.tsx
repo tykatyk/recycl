3cdb7e2202ebf26881ae01899d243e9f835339c1
tykatyk <tykatyk@gmail.com>
2023-09-22 15:24:55 +0300

    Refactor Time field
    To correctly handle value changes

--- components/uiParts/formInputs/Time.tsx
+++ components/uiParts/formInputs/Time.tsx
@@ -1,14 +1,30 @@
 import { LocalizationProvider, TimePicker } from '@mui/x-date-pickers'
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs'
+import { TextFieldProps, fieldToTextField } from 'formik-mui'
+import { useEffect } from 'react'
 
 type TimeProps = {
   label: string
 }
-export const Time = ({ label = 'Время' }: TimeProps) => {
+export const Time = (props: TextFieldProps) => {
+  const {
+    form: { setFieldValue, setFieldTouched },
+    field: { name },
+  } = props
+  const { label, error, helperText, value, fullWidth } = fieldToTextField(props)
+
+  useEffect(() => {
+    setFieldTouched(name)
+  }, [value])
+
   return (
     <LocalizationProvider dateAdapter={AdapterDayjs} adapterLocale="ru">
       <TimePicker
         label={label}
+        onChange={(newValue) => {
+          setFieldValue(name, newValue, false)
+        }}
+        // disablePast
         slotProps={{
           popper: {
             sx: {
@@ -16,6 +32,12 @@ export const Time = ({ label = 'Время' }: TimeProps) => {
                 bgcolor: 'secondary.main',
               },
             },
+          },
+          textField: {
+            fullWidth,
+            helperText,
+            error: !!error,
+            name: name,
           },
         }}
       />
