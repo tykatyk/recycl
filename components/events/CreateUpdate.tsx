df9e925f2805fd23f60cab953b4ce4ce4cc7b84f
tykatyk <tykatyk@gmail.com>
2023-10-02 10:19:44 +0300

    Refactor event CreateUpdate component

--- components/events/CreateUpdate.tsx
+++ components/events/CreateUpdate.tsx
@@ -1,27 +1,21 @@
 import React from 'react'
-import { Grid } from '@mui/material'
 import Layout from '../layouts/Layout'
 import EventForm from './EventForm'
 import RedirectUnathenticatedUser from '../uiParts/RedirectUnathenticatedUser'
-import { EventId } from '../../lib/types/event'
+import { InitialEventData } from '../../lib/types/event'
 
 //ToDo: seems like tihs component can be separated to prevent code duplication
 //since it's used also in removal applications
-export default function CreateUpdateEvent(props: EventId) {
+export default function CreateUpdateEvent(props: InitialEventData) {
+  const { event } = props
+  const title = event
+    ? 'Редактировать объявление о вывозе отходов | Recycl'
+    : 'Создать объявление о вывозе отходов | Recycl'
+
   return (
     <RedirectUnathenticatedUser>
-      <Layout title="Создать объявление о вывозе отходов | Recycl">
-        <Grid
-          container
-          direction="column"
-          style={{
-            maxWidth: '750px',
-            margin: '0 auto',
-            padding: '16px',
-          }}
-        >
-          <EventForm {...props} />
-        </Grid>
+      <Layout title={title}>
+        <EventForm {...props} />
       </Layout>
     </RedirectUnathenticatedUser>
   )
