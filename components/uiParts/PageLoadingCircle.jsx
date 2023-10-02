e2dd6109e407b269642c0792153d98ea06696f37
tykatyk <tykatyk@gmail.com>
2023-10-02 10:21:02 +0300

    Refactor loading indicator
    to show in the center of the screen

--- components/uiParts/PageLoadingCircle.jsx
+++ components/uiParts/PageLoadingCircle.jsx
@@ -1,21 +1,22 @@
 import React from 'react'
-import { CircularProgress } from '@mui/material'
-import { css } from '@emotion/react'
-
-const wrapperCss = css({
-  position: 'fixed',
-  top: ' 50%',
-  left: '50%',
-  transform: 'translate(-50%, -50%)',
-})
+import { Box, CircularProgress } from '@mui/material'
 
 export default function PageLoadingCircle(props) {
-  const { size = 40, className = undefined } = props
-  const css = className ? {} : wrapperCss
+  const { size = 40, sx = [] } = props
 
   return (
-    <div css={css} className={className}>
+    <Box
+      sx={[
+        {
+          position: 'fixed',
+          top: ' 50%',
+          left: '50%',
+          transform: 'translate(-50%, -50%)',
+        },
+        ...(Array.isArray(sx) ? sx : [sx]),
+      ]}
+    >
       <CircularProgress size={size} color="secondary" />
-    </div>
+    </Box>
   )
 }
