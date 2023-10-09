a51e030382a4438f2a6dbf741c56f8501baaab32
tykatyk <tykatyk@gmail.com>
2023-10-09 22:06:15 +0300

    Fix incorrect error handling

--- lib/db/queries/event.ts
+++ lib/db/queries/event.ts
@@ -1,7 +1,5 @@
-import getCoords from '../../helpers/getCoords'
-//import removalApplication from '../../validation/removalApplication' //ToDo: Add validation before create and update operations
+//ToDo: Add validation before create and update operations
 import { Event } from '../models/index'
-import { internalServerError } from '../../errors/index'
 import mongoose from 'mongoose'
 
 const eventQueries = {
@@ -10,78 +8,48 @@ const eventQueries = {
 
     data.user = {}
 
-    try {
-      data.user._id = user['_id']
-      data.user.name = user.name
+    data.user._id = user['_id']
+    data.user.name = user.name
 
-      const event = new Event(data)
-      return await event.save()
-    } catch (error) {
-      console.log(error)
-      throw internalServerError
-    }
+    const event = new Event(data)
+    return await event.save()
   },
 
   get: async (id) => {
-    try {
-      const result = await Event.findById(id, {
-        __v: 0,
-        user: 0,
-        isActive: 0,
-        expires: 0,
-        createdAt: 0,
-        updatedAt: 0,
-      }).exec()
-      return result
-    } catch (error) {
-      console.log(error)
-      // throw internalServerError
-    }
+    const result = await Event.findById(id, {
+      __v: 0,
+      user: 0,
+      isActive: 0,
+      expires: 0,
+      createdAt: 0,
+      updatedAt: 0,
+    }).exec()
+    return result
   },
 
   getAll: async (queryParams, user) => {
-    try {
-      if (!user) return null
-      const query = {}
+    if (!user) return null
+    const query = {}
 
-      if (queryParams.wasteType)
-        query['wasteType'] = new mongoose.Types.ObjectId(queryParams.wasteType)
-      if (queryParams.city) query['wasteLocation.place_id'] = queryParams.city
+    if (queryParams.wasteType)
+      query['wasteType'] = new mongoose.Types.ObjectId(queryParams.wasteType)
+    if (queryParams.city) query['wasteLocation.place_id'] = queryParams.city
 
-      return await Event.find(query).populate('wasteType')
-    } catch (error) {
-      console.log(error)
-      throw internalServerError
-    }
+    return await Event.find(query).populate('wasteType')
   },
 
-  update: async (id, newValue) => {
-    try {
-      return await Event.findByIdAndUpdate(id, newValue, {
-        new: true,
-      }).exec()
-    } catch (error) {
-      console.log(error)
-      throw internalServerError
-    }
+  update: async (newValue) => {
+    return await Event.findByIdAndUpdate(newValue._id, newValue, {
+      new: true,
+    }).exec()
   },
 
   delete: async (id) => {
-    try {
-      return await Event.findByIdAndRemove(id).exec()
-    } catch (error) {
-      console.log(error)
-      throw internalServerError
-    }
+    return await Event.findByIdAndRemove(id).exec()
   },
 
   deleteMany: async (ids) => {
-    try {
-      return await Event.deleteMany({ _id: { $in: ids } }).exec()
-    } catch (error) {
-      console.log(error)
-      throw internalServerError
-    }
+    return await Event.deleteMany({ _id: { $in: ids } }).exec()
   },
 }
 export default eventQueries
