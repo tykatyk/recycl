164c6fd04ebd73621733cd4e5be1a9c7ea43668e
tykatyk <tykatyk@gmail.com>
2023-11-01 15:15:01 +0200

    Fix query to serve active events

---
+++ lib/db/queries/eventQuery.ts
@@ -0,0 +1,101 @@
+//ToDo: Add validation before create and update operations
+import { Event } from '../models/index'
+import mongoose from 'mongoose'
+import user from './user'
+import { _id } from '@next-auth/mongodb-adapter'
+
+const eventQueries = {
+  create: async (data, user) => {
+    if (!user) return null
+
+    data.user = {}
+
+    data.user._id = user['_id']
+    data.user.name = user.name
+
+    const event = new Event(data)
+    return await event.save()
+  },
+
+  get: async (id) => {
+    const result = await Event.findById(id, {
+      __v: 0,
+      user: 0,
+      isActive: 0,
+      expires: 0,
+      createdAt: 0,
+      updatedAt: 0,
+    }).exec()
+    return result
+  },
+
+  getAll: async (queryParams, user) => {
+    if (!user) return { total: 0, data: [] }
+
+    const {
+      variant = 'active',
+      direction = 'forward',
+      offset = '',
+      pageSize = 0,
+    } = queryParams
+
+    type EventQuery = {
+      'user._id': mongoose.Types.ObjectId
+      _id?: {
+        $lt?: mongoose.Types.ObjectId
+        $gt?: mongoose.Types.ObjectId
+      }
+      isActive: Boolean
+    }
+
+    const query: EventQuery | {} = {}
+
+    query['user._id'] = new mongoose.Types.ObjectId(user)
+
+    if (offset) {
+      if (direction === 'forward') {
+        query['_id'] = {
+          $lt: new mongoose.Types.ObjectId(queryParams.offset),
+        }
+      }
+
+      if (direction === 'backward') {
+        query['_id'] = {
+          $gt: new mongoose.Types.ObjectId(queryParams.offset),
+        }
+      }
+    }
+
+    if (variant === 'active') {
+      query['isActive'] = true
+      query['date'] = {
+        $gte: new Date(),
+      }
+    } else {
+      query['$or'] = [{ isActive: false }, { date: { $lt: new Date() } }]
+    }
+
+    const events = await Event.find(query).limit(pageSize).populate('waste')
+
+    const totalItems = await Event.countDocuments({
+      'user._id': query['user._id'],
+    })
+
+    return { total: totalItems, events }
+  },
+
+  update: async (newValue) => {
+    return await Event.findByIdAndUpdate(newValue._id, newValue, {
+      new: true,
+    }).exec()
+  },
+
+  delete: async (id) => {
+    return await Event.findByIdAndRemove(id).exec()
+  },
+
+  deleteMany: async (ids) => {
+    return await Event.deleteMany({ _id: { $in: ids } }).exec()
+  },
+}
+export default eventQueries
