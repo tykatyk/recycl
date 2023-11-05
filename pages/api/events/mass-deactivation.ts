aeee41dff8d52b745156bd0e24cb82be4c857a6f
tykatyk <tykatyk@gmail.com>
2023-11-05 17:09:40 +0200

    Add event mass deactivation route

---
+++ pages/api/events/mass-deactivation.ts
@@ -0,0 +1,47 @@
+import { getServerSession } from 'next-auth/next'
+import { authOptions } from '../auth/[...nextauth]'
+import { NextApiRequest, NextApiResponse } from 'next'
+import eventModel from '../../../lib/db/models/eventModel'
+import dbConnect from '../../../lib/db/connection'
+
+export default async function EventMassDeactivation(
+  req: NextApiRequest,
+  res: NextApiResponse,
+) {
+  const session = await getServerSession(req, res, authOptions)
+
+  if (!session) {
+    res.status(401).end()
+    return
+  }
+
+  if (req.method === 'POST') {
+    await dbConnect()
+    console.log(req.body)
+    const { eventIds }: { eventIds: string[] } = req.body
+
+    if (!Array.isArray(eventIds) || !(eventIds.length > 0)) {
+      res
+        .status(400)
+        .json({ error: `Expected an array of ids but got ${eventIds}` })
+      return
+    }
+
+    try {
+      await eventModel.updateMany(
+        { user: session.id, _id: { $in: eventIds } },
+        { isActive: false },
+      )
+    } catch (e) {
+      console.log(e)
+      res
+        .status(500)
+        .json({ error: 'An error occurred while inactivating ads' })
+      return
+    }
+
+    res.status(200).json({ message: 'Ads successfully deactivated' })
+  } else {
+    res.status(405).json({ error: 'Method not allowed' })
+  }
+}
