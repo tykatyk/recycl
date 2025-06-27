901ed2dbaeed07e4eb773a2f7a5a2940b5ac8749
tykatyk <tykatyk@gmail.com>
2025-06-27 15:51:37 +0300

    Add dispatcher

--- lib/jobs/notifyRemovalEventSubsriptionsJob.ts
+++ lib/jobs/notifyRemovalEventSubsriptionsJob.ts
@@ -1,6 +1,5 @@
 import dbConnect from '../db/connection'
 import removalEventModel from '../db/models/eventModel'
-import removalEventNotificationModel from '../db/models/removalEventNotification'
 import removalEventSubscriptionModel from '../db/models/removalEventSubscription'
 import dayjs from 'dayjs'
 import duration from 'dayjs/plugin/duration' // ES 2015
@@ -8,88 +7,115 @@ import sendpulse from 'sendpulse-api'
 
 dayjs.extend(duration)
 
-const hourLimit = 2500 //email за годину
-const minuteLimit = 1000 //запитів за хвилину
-const dispatcher = {
-  canSendNext: true,
+class Dispatcher {
+  hourLimit: number
+  minuteLimit: number
+  timestamps: number[]
+  queue: any[]
+  timeout: any
+
+  constructor() {
+    this.hourLimit = 2500 //запитів за годину
+    this.minuteLimit = 1000 //запитів за хвилину
+    this.timestamps = []
+    this.queue = []
+    this.timeout = null
+  }
+
+  canSendNext() {
+    this.timestamps = this.timestamps.filter((timestamp) => {
+      Date.now() - timestamp <= 3600 * 1000
+    })
+    const lastMinuteCount = this.timestamps.filter((timestamp) => {
+      Date.now() - timestamp <= 60 * 1000
+    }).length
+    const lastHourCount = this.timestamps.length
+    return lastMinuteCount < this.minuteLimit && lastHourCount < this.hourLimit
+  }
+
+  addTask(task: any) {
+    this.queue.push(task)
+    this.processTask()
+  }
+
+  processTask() {
+    if (this.queue.length === 0) {
+      clearTimeout(this.timeout)
+      return
+    }
+
+    if (this.canSendNext()) {
+      const task = this.queue.shift()
+      this.timestamps.push(Date.now())
+      task()
+    }
+
+    clearTimeout(this.timeout)
+
+    this.timeout = setTimeout(() => {
+      this.processTask()
+    }, 100)
+  }
 }
 
-export async function notify() {
+export async function processSubscriptions() {
   await dbConnect()
-  const startingQuery = await getStartingQuery()
-
   const subscriptionCursor = removalEventSubscriptionModel
     .find({ isActive: true })
     .cursor()
+  const dispatcher = new Dispatcher()
 
-  processSubscriptions(subscriptionCursor, startingQuery)
-}
+  //Орієнтовна структура елемента масиву в  notification.locations
+  /*const obj = {
+    locationName,
+    removalEvents = [
+      { wasteName, agents: [{ removalAgent, eventDate }, {}, {}] },
+    ],
+  }*/
 
-async function getStartingQuery() {
-  //retrieve last processed event
-  const removalEventNotification = await removalEventNotificationModel
-    .findOne()
-    .sort({ _id: -1 })
+  await subscriptionCursor.eachAsync(async (subscription) => {
+    subscription.populate('user', 'email')
+    const notification: any = {}
+    notification.receiver = subscription.user
+    notification.locations = []
 
-  const lastProcessedEventId = removalEventNotification.lastProcessedEvent
+    //Підписка складається з елементів. Кожен елемент це окремий населений пункт
+    for (const element of subscription.elements) {
+      const { city, wasteTypes } = element
 
-  const startingQuery = lastProcessedEventId
-    ? {
-        _id: { $gt: lastProcessedEventId },
+      const eventsPerLocation: any = {}
+      eventsPerLocation.locationName = city
+      eventsPerLocation.removalEvents = []
+
+      //В кожному населеному пункті користувач може бути підписаний на вивіз різних типів відходів
+      for (const wasteType in wasteTypes) {
+        const removalEventsCursor = removalEventModel
+          .find({ wasteType, city, date: { $gte: Date.now() }, isActive: true })
+          .cursor()
+
+        const agents: any[] = []
+
+        //Певний тип відходів може вивозитись різними переробниками, які публікують оголошення про вивіз відходів (removalEvent)
+        for (
+          //ці івенти бажано закинути одразу в redis і потім витягувати з кешу для підвищення продуктивності
+          let ev = await removalEventsCursor.next(), counter = 0;
+          ev != null && counter <= 2;
+          ev = await removalEventsCursor.next(), counter++
+        ) {
+          const { user: removalAgent, date, phone } = ev
+          agents.push({ removalAgent, date, phone })
+        }
+
+        const eventstPerWasteType: any = {}
+        eventstPerWasteType.wasteName = wasteType
+        eventstPerWasteType.agents = agents
+
+        eventsPerLocation.removalEvents.push(eventstPerWasteType)
       }
-    : {
-        date: { $gte: dayjs() },
-      }
-
-  return startingQuery
-}
-
-async function processSubscriptions(subscriptionCursor, startingQuery) {
-  if (!dispatcher.canSendNext) {
-    setTimeout(async () => {
-      await processSubscriptions(subscriptionCursor, startingQuery)
-    }, 100)
-    return
-  }
-
-  const subscription = await subscriptionCursor.next()
-
-  if (!subscription) return
-
-  subscription.populate('user', 'email')
-  const notification: any = {}
-  notification.receiver = subscription.user
-  notification.locations = []
-  const removalEventsPerCity: any = {}
-
-  //користувач може бути підписаний на отримання сповіщень про вивезення різних видів відходів в різних населених пунктах
-  for (const element of subscription.elements) {
-    const { city, wasteTypes } = element
-    const removalEventsPerWasteType: any = {}
-
-    for (const wasteType in wasteTypes) {
-      const query = { ...startingQuery, city, wasteType, isActive: true }
-      const removalEventsCursor = removalEventModel.find(query).cursor()
-      const removalEvents: any[] = []
-
-      //в кожному населеному пункті певний тип відходів може вивозитись декількома переробниками
-      for (
-        //ці івенти бажано закинути одразу в redis і потім витягувати з кешу для підвищення продуктивності
-        let removalEvent = await removalEventsCursor.next(), counter = 0;
-        removalEvent != null && counter <= 2;
-        removalEvent = await removalEventsCursor.next(), counter++
-      ) {
-        const { user: removalAgent, date, phone } = removalEvent
-        removalEvents.push({ removalAgent, date, phone })
-      }
-
-      removalEventsPerWasteType.wasteType = removalEvents
+      notification.locations.push(eventsPerLocation)
     }
-    removalEventsPerCity.city = removalEventsPerWasteType
-  }
-  notification.locations = removalEventsPerCity
-  sendEmail(notification)
-  await processSubscriptions(subscriptionCursor, startingQuery)
+    dispatcher.addTask(sendEmail(notification))
+  })
 }
 
 function sendEmail(notification) {}
