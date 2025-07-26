ac69ee274ada36048efb27ec561059a15625edca
tykatyk <tykatyk@gmail.com>
2025-07-26 17:54:59 +0300

    Redraw secondary color

--- lib/helpers/themeStub.ts
+++ lib/helpers/themeStub.ts
@@ -6,27 +6,30 @@ import type { ThemeColors } from '../types/themeColors'
 export const colors: ThemeColors = {
   primary: '#1a2b34', // dark blue-green
   secondary: '#f8bc45', // yellow
+  // secondary: '#adce5d', // lime
   background: '#223c4a', // blue-green
   brand: '#adce5d',
   text: '#ffffff',
   buttonText: '#1f1f1f',
 }
 
-const theme = createTheme({
-  palette: {
-    mode: 'dark',
-    primary: {
-      main: colors.primary,
-    },
-    secondary: {
-      main: colors.secondary,
-    },
-    background: {
-      paper: '#2a363c', //dark blue-grey
-      default: colors.background,
+const theme = createTheme(
+  {
+    palette: {
+      mode: 'dark',
+      primary: {
+        main: colors.primary,
+      },
+      secondary: {
+        main: colors.secondary,
+      },
+      background: {
+        paper: '#2a363c', //dark blue-grey
+        default: colors.background,
+      },
     },
   },
   coreRuRu,
   ruRU,
-})
+)
 export default theme
