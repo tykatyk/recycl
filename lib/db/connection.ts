cb4cf58a05fb324771763afd9d7baaab37313375
tykatyk <tykatyk@gmail.com>
2025-08-17 12:23:40 +0300

    Add check for database url in connection

--- lib/db/connection.ts
+++ lib/db/connection.ts
@@ -1,16 +1,22 @@
 import mongoose from 'mongoose'
 
 export default async function dbConnect() {
+  const uri = process.env.DATABASE_URL
+  if (!uri) {
+    console.log('Database url is not defined in environment variables')
+    return
+  }
+
   if (!mongoose.connections[0].readyState) {
     // Use new db connection
     await mongoose
-      .connect(process.env.DATABASE_URL)
+      .connect(uri)
       .then(() => {
         console.log('Connected to the database')
       })
       .catch((err) => {
-        console.log('Cannot connect to the database', err)
-        process.exit()
+        console.log('Cannot connect to the database\n', err)
+        process.exit(1)
       })
   }
 }
