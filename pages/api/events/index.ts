0c98b9b7742b87e48173fb15b4cdeeb0d6c73dd4
tykatyk <tykatyk@gmail.com>
2025-08-18 13:56:19 +0300

    Add query validation

--- pages/api/events/index.ts
+++ pages/api/events/index.ts
@@ -1,35 +1,74 @@
 import { getServerSession } from 'next-auth/next'
 import { authOptions } from '../auth/[...nextauth]'
 import { NextApiRequest, NextApiResponse } from 'next'
-import { eventValidationSchema } from '../../../lib/validation/eventFormValidator'
 import eventQueries from '../../../lib/db/queries/eventQuery'
-import eventModel from '../../../lib/db/models/eventModel'
-import {
-  errorResponse,
-  perFormErrorResponse,
-} from '../../../lib/helpers/responses'
 import dbConnect from '../../../lib/db/connection'
-import type { IsInactive } from '../../../lib/types/event'
-import mongoose from 'mongoose'
+import * as yup from 'yup'
+import {
+  rowsPerPageOptions,
+  validOrderBy,
+  validSortOrder,
+  eventVariants,
+} from '../../../lib/helpers/eventHelpers'
+import { Variant, SortOrder, OrderBy } from '../../../lib/types/event'
 
 export default async function Events(
   req: NextApiRequest,
   res: NextApiResponse,
 ) {
-  if (req.method === 'GET') {
-    //check if user is authenticated
-    const session = await getServerSession(req, res, authOptions)
-    if (!session || !session.id) {
-      res.status(401).end()
-      return
-    }
+  if (req.method !== 'GET')
+    return res.status(405).json({ error: 'Method not allowed' })
 
-    const userId = session.id
+  //check if user is authenticated
+  const session = await getServerSession(req, res, authOptions)
+  if (!session?.id) return res.status(401).end()
+
+  const userId = session.id
+
+  const querySchema = yup.object({
+    page: yup
+      .number()
+      .transform((value) => (value === '' || isNaN(value) ? undefined : value))
+      .integer()
+      .min(0)
+      .default(0),
+    pageSize: yup
+      .number()
+      .transform((value) => (value === '' || isNaN(value) ? undefined : value))
+      .integer()
+      .min(rowsPerPageOptions[0])
+      .max(rowsPerPageOptions[Math.max(rowsPerPageOptions.length - 1, 0)])
+      .default(rowsPerPageOptions[0]),
+    sortOrder: yup
+      .string<SortOrder>()
+      .transform((value) =>
+        validSortOrder.indexOf(value) >= 0 ? value : undefined,
+      )
+      .default('desc'),
+    sortProperty: yup
+      .string<OrderBy>()
+      .transform((value) =>
+        validOrderBy.indexOf(value) >= 0 ? value : undefined,
+      )
+      .default('createdAt'),
+    variant: yup
+      .string<Variant>()
+      .transform((value) => (eventVariants[value] ? value : undefined))
+      .default(eventVariants.active),
+  })
+
+  try {
+    let validatedQuery = await querySchema.validate(req.query, {
+      stripUnknown: true,
+    })
     await dbConnect()
-
-    const data = await eventQueries.getAll(req.query, userId)
-    res.json(data)
-  } else {
-    res.status(405).json({ error: 'Method not allowed' })
+    const data = await eventQueries.getAll(validatedQuery, userId)
+    return res.json(data)
+  } catch (err) {
+    if (err instanceof yup.ValidationError) {
+      return res.status(400).json({ error: err.errors })
+    }
+    console.log(err)
+    return res.status(500).json({ error: 'Internal server error' })
   }
 }
