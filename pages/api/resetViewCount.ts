15cdcdaecad48d62c62e50bc05092ee2b858feb9
tykatyk <tykatyk@gmail.com>
2025-09-01 17:32:52 +0300

    Add route to reset view count of the ads

---
+++ pages/api/resetViewCount.ts
@@ -0,0 +1,43 @@
+import { getServerSession } from 'next-auth/next'
+import { authOptions } from './auth/[...nextauth]'
+import { NextApiRequest, NextApiResponse } from 'next'
+import eventModel from '../../lib/db/models/eventModel'
+import dbConnect from '../../lib/db/connection'
+import {
+  apiHandler,
+  INTERNAL_SERVER_ERROR,
+} from '../../lib/helpers/errorHelpers'
+
+async function resetViewCount(req: NextApiRequest, res: NextApiResponse) {
+  if (req.method !== 'POST') {
+    return res.status(405).json({ error: 'Method not allowed' })
+  }
+
+  const session = await getServerSession(req, res, authOptions)
+  if (!session?.id) return res.status(401).end()
+
+  await dbConnect()
+
+  const { adType, adId } = req.body
+
+  switch (adType) {
+    case 'event':
+      const ad = await eventModel.findOneAndUpdate(
+        {
+          _id: adId,
+          user: session.id,
+        },
+        { viewCount: 0, viewedBy: [] },
+      )
+      if (!ad) {
+        return res.status(404).json({ error: 'Document not found' })
+      }
+
+      return res.status(200).json({ success: true })
+
+    default:
+      res.status(500).json({ error: INTERNAL_SERVER_ERROR })
+  }
+}
+
+export default apiHandler(resetViewCount)
