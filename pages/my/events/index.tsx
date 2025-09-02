cb72d88316a57b5dc03704990d679a73299f47aa
tykatyk <tykatyk@gmail.com>
2025-09-02 14:51:34 +0300

    Add authentication to the route

--- pages/my/events/index.tsx
+++ pages/my/events/index.tsx
@@ -1,15 +1,35 @@
+import { getSession } from 'next-auth/react'
 import MyEvents from '../../../components/events/MyEvents'
 import { Variant } from '../../../lib/types/event'
 import type { InferGetServerSidePropsType, GetServerSideProps } from 'next'
 
-export default function ShowMyEvents({
+export default function MyEventsIndexPage({
   variant,
 }: InferGetServerSidePropsType<typeof getServerSideProps>) {
   return <MyEvents variant={variant} />
 }
 
-export const getServerSideProps = (async () => {
+export const getServerSideProps = (async (context) => {
+  const { resolvedUrl, req, query } = context
+
+  const session = await getSession({ req })
+
+  if (!session?.user)
+    return {
+      redirect: {
+        permanent: false,
+        destination: `/auth/login?from=${encodeURIComponent(resolvedUrl)}`,
+      },
+    }
+
+  const { variant } = query
+
+  const validatedVariant: Variant =
+    variant === 'inactive' ? 'inactive' : 'active'
+
   return {
-    props: { variant: 'active' },
+    props: {
+      variant: validatedVariant,
+    },
   }
 }) satisfies GetServerSideProps<{ variant: Variant }>
