314fd02903a2df1c35280fc8768954f9d63048b7
tykatyk <tykatyk@gmail.com>
2025-09-03 14:46:09 +0300

    Refactor Session type to have an id

--- lib/types/next-auth.d.ts
+++ lib/types/next-auth.d.ts
@@ -5,8 +5,8 @@ declare module 'next-auth' {
   /**
    * Returned by `useSession`, `getSession` and received as a prop on the `SessionProvider` React Context
    */
-  export interface Session {
-    id?: string
+  export interface Session extends DefaultSession {
+    id: string
   }
 }
 
