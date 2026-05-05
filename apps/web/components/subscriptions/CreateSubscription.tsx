fc58b0bf67dfcc52b58ef9fb48d5ef6f45606d5f
tykatyk <tykatyk@gmail.com>
2026-05-05 16:56:02 +0300

    Add form to create wasteAvailable subscription

--- apps/web/components/subscriptions/CreateSubscription.tsx
+++ apps/web/components/subscriptions/CreateSubscription.tsx
@@ -1,6 +1,6 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import Snackbar from '../uiParts/Snackbars'
-import Error from '../uiParts/Error'
+import ErrorComponent from '../uiParts/Error'
 import SubscriptionForm from './SubscriptionForm'
 import { Formik, FormikHelpers } from 'formik'
 import {
@@ -17,8 +17,16 @@ import type {
 import { useRouter } from 'next/router'
 import { useSession } from 'next-auth/react'
 import { Box, Typography } from '@mui/material'
-import user from '../../../../packages/db/models/user'
+import type { Waste } from '../../lib/types/waste'
+import { PlaceType } from '../../lib/types/placeAutocomplete'
+import Layout from '../layouts/Layout'
 
+type WasteAvailableSubscriptionConfig = {
+  location: PlaceType | null
+  wasteTypes: Waste[]
+}
+
+const title = 'Добавление подписки на получение уведомлений о наличии вторсирья'
 const errorMessage = 'Возникла ошибка при сохранении заявки'
 const api = '/api/events'
 const createRoute = `${api}/create`
@@ -26,33 +34,60 @@ const updateRoute = (id: string) => `${api}/${id}`
 const indexRoute = '/my/events'
 const inactiveEventsRoute = '/my/events/inactive'
 
-export default function CreateSubscription(props: EventCreateUpdateProps) {
-  const { event, userPhone, wasteTypes } = props
+export default function CreateSubscription() {
   const [severity, setSeverity] = useState<string>('success')
+  const [wasteTypes, setWasteTypes] = useState<Waste[]>([])
   const router = useRouter()
   const { isInactive }: IsInactive = router.query
   const [notification, setNotification] = useState<string>('')
-  const initialValues = getInitialValues(event, userPhone)
+  // const initialValues = getInitialValues(event, userPhone)
   const { data: session, status } = useSession({
     required: true,
     onUnauthenticated() {
       window.location.replace('/auth/login?from=/my/subscriptions/create')
     },
   })
-  console.log(session)
-  console.log(status)
+
+  const getWasteTypes = async () => {
+    return await fetch('/api/waste-types')
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error('Response is not OK')
+        }
+        return response.json()
+      })
+      .then((data: Waste[]) => {
+        return data
+      })
+  }
+
+  useEffect(() => {
+    getWasteTypes()
+      .then((wasteTypes) => {
+        setWasteTypes(wasteTypes)
+      })
+      .catch((error) => {
+        setSeverity('error')
+        setNotification('Oшибка сервера')
+      })
+  }, [])
 
   //show error if no wasteTypes found
-  if (!wasteTypes) return <Error />
+  if (!wasteTypes) return <ErrorComponent />
 
   //ToDo: refactor to helper function, since this handler can also be used for creating removalApplications
   const createHandler = (
-    values: Event,
-    { setSubmitting, setErrors, resetForm }: FormikHelpers<Event>,
+    values: WasteAvailableSubscriptionConfig,
+    {
+      setSubmitting,
+      setErrors,
+      resetForm,
+    }: FormikHelpers<WasteAvailableSubscriptionConfig>,
   ) => {
     setSubmitting(true)
 
-    const normalizedValues = getNormalizedValues(values)
+    // const normalizedValues = getNormalizedValues(values)
+    const normalizedValues = null
 
     fetch(createRoute, {
       method: 'POST',
@@ -81,28 +116,49 @@ export default function CreateSubscription(props: EventCreateUpdateProps) {
   }
 
   return (
-    <Box>
-      <Typography paragraph variant="h4">
-        Объявление о вывозе отходов
-      </Typography>
-      <Formik
-        enableReinitialize
-        initialValues={initialValues}
-        validationSchema={eventSchema}
-        onSubmit={(values: Event, actions: FormikHelpers<Event>) => {
-          createHandler(values, actions)
+    <Layout title={title}>
+      <Box
+        sx={{
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'center',
+          alignItems: 'center',
         }}
       >
-        <SubscriptionForm wasteTypes={wasteTypes} />
-      </Formik>
-      <Snackbar
-        severity={severity}
-        open={!!notification}
-        message={notification}
-        handleClose={() => {
-          setNotification('')
-        }}
-      />
-    </Box>
+        <Typography
+          component={'h1'}
+          variant="h6"
+          paragraph
+          align="center"
+          sx={{ mb: 3 }}
+        >
+          {title}
+        </Typography>
+        <Box sx={{ width: '100%' }}>
+          <Formik
+            enableReinitialize
+            initialValues={{ location: null, wasteTypes: [] }}
+            // validationSchema={eventSchema}
+            onSubmit={(
+              values: WasteAvailableSubscriptionConfig,
+              actions: FormikHelpers<WasteAvailableSubscriptionConfig>,
+            ) => {
+              createHandler(values, actions)
+            }}
+          >
+            <SubscriptionForm wasteTypes={wasteTypes} />
+          </Formik>
+        </Box>
+
+        <Snackbar
+          severity={severity}
+          open={!!notification}
+          message={notification}
+          handleClose={() => {
+            setNotification('')
+          }}
+        />
+      </Box>
+    </Layout>
   )
 }
