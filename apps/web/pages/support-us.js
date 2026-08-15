3a8038250b57fbfc23fdd38f1b889da8c753eddc
tykatyk <tykatyk@gmail.com>
2026-08-15 13:13:49 +0300

    Redraw support-us page

--- apps/web/pages/support-us.js
+++
@@ -1,6 +0,0 @@
-import React from 'react'
-import SupportUsPage from '../components/SupportUsPage'
-
-export default function SupportUs() {
-  return <SupportUsPage />
-}
---
+++ apps/web/pages/support-us.tsx
@@ -0,0 +1,57 @@
+import { Typography, Box, Paper } from '@mui/material'
+import Layout from '../components/layouts/Layout'
+
+const btcWallet = '6Le-cZ8dAAAAABgRwLZP_IVBeV8ZJueinte6rm5n'
+const ethWallet = '2lkadg9xlkj#laclk98255xkjagsdf'
+const usdtWallet = 'aosudifhsadjkfhasdfmasdf'
+
+const brand = process.env.NEXT_PUBLIC_BRAND || ''
+const h1 = 'Настройки аккаунта'
+const title = `${h1} | ${brand}`
+
+export default function SupportUsPage() {
+  return (
+    <Layout title={title}>
+      <Box
+        sx={{
+          display: 'flex',
+          flexDirection: 'column',
+        }}
+      >
+        <Typography component="h1" variant="h5" align="center" sx={{ mb: 5 }}>
+          Если вы хотите помочь проекту, то можете сделать это одним из
+          перечисленных ниже способов
+        </Typography>
+        <Paper>
+          <Box
+            sx={{
+              p: 2,
+              '&>*': {
+                mb: 3,
+              },
+            }}
+          >
+            <Box>
+              <Typography variant="h6" align="center">
+                Bitcoin
+              </Typography>
+              <Typography align="center">{btcWallet}</Typography>
+            </Box>
+            <Box>
+              <Typography align="center" variant="h6">
+                Etherum
+              </Typography>
+              <Typography align="center">{ethWallet}</Typography>
+            </Box>
+            <Box>
+              <Typography align="center" variant="h6">
+                USDT
+              </Typography>
+              <Typography align="center">{usdtWallet}</Typography>
+            </Box>
+          </Box>
+        </Paper>
+      </Box>
+    </Layout>
+  )
+}
