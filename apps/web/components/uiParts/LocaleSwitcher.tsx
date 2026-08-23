c40943da2156d67a150682e03fc1465bc4bca7c4
tykatyk <tykatyk@gmail.com>
2026-08-23 12:20:12 +0300

    Refactor locale switcher

--- apps/web/components/uiParts/LocaleSwitcher.tsx
+++ apps/web/components/uiParts/LocaleSwitcher.tsx
@@ -2,6 +2,16 @@ import { useRouter } from 'next/router'
 import { useTranslations } from 'next-intl'
 import Link from './Link'
 import Cookies from 'js-cookie'
+import { Box } from '@mui/material'
+
+const css = {
+  color: '#fff',
+  fontSize: 12,
+  textDecoration: 'none',
+  '&:hover': {
+    textDecoration: 'underline',
+  },
+}
 
 export default function LocaleSwitcher() {
   const t = useTranslations('LocaleSwitcher')
@@ -13,27 +23,32 @@ export default function LocaleSwitcher() {
     return null
   }
 
-  const handleClick = () => {
-    Cookies.set('NEXT_LOCALE', otherLocale, {
+  const handleClick = (locale) => {
+    Cookies.set('NEXT_LOCALE', locale, {
       expires: 365,
       path: '/',
     })
   }
 
   return (
-    <Link
-      href={route}
-      locale={otherLocale}
-      onClick={handleClick}
-      sx={{
-        color: '#fff',
-        textDecoration: 'none',
-        '&:hover': {
-          textDecoration: 'underline',
-        },
-      }}
-    >
-      {t('switchLocale', { locale: locale as string })}
-    </Link>
+    <Box sx={{ display: 'flex', flexDirection: 'column' }}>
+      {locales?.map((localeItem, idx) => {
+        return (
+          <Box key={idx}>
+            <Link
+              href={route}
+              locale={localeItem}
+              onClick={() => handleClick(localeItem)}
+              sx={{
+                ...css,
+                textDecoration: localeItem === locale ? 'underline' : 'none',
+              }}
+            >
+              {t('switchLocale', { locale: localeItem })}
+            </Link>
+          </Box>
+        )
+      })}
+    </Box>
   )
 }
