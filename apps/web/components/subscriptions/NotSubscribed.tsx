b6638ef6e56e0532cdda72be204d6bf1c35d3484
tykatyk <tykatyk@gmail.com>
2026-09-10 14:15:28 +0300

    Ad translations

--- apps/web/components/subscriptions/NotSubscribed.tsx
+++ apps/web/components/subscriptions/NotSubscribed.tsx
@@ -1,19 +1,18 @@
 import { Box, Typography } from '@mui/material'
 import Link from '../uiParts/Link'
+import { useTranslations } from 'next-intl'
 
 export default function NotSubscribed(params: { message: string }) {
+  const t = useTranslations('CreateUpdateWasteAvailableSubscription')
+
   return (
     <Box
       sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}
     >
-      <Typography paragraph align="center">
-        {params.message}
-      </Typography>
-      <Typography paragraph align="center">
-        Чтобы добавить подписку, включите уведомления, перейдя по ссылке:
-      </Typography>
+      <Typography align="center">{params.message}</Typography>
+      <Typography align="center">{`${t('enableNotifications')}:`}</Typography>
       <Typography>
-        <Link href="/my/subscriptions">Управлять подписками</Link>
+        <Link href="/my/subscriptions">{t('configureSubscriptions')}</Link>
       </Typography>
     </Box>
   )
